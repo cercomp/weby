a09e5a49ab5ce57fe88ad5e52a0e6d57e757bdda
Marcello Henrique <faraohh@gmail.com>
2010-07-06 21:46:00 +0000

    add arquivo faltante.

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,87 @@
+class SitesController < ApplicationController
+  layout :choose_layout
+  before_filter :require_user, :except => [:show, :index]
+  before_filter :check_authorization, :except => [:show, :index]
+  
+  # GET /sites
+  # GET /sites.xml
+  def index
+    @sites = Site.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @sites }
+    end
+  end
+
+  # GET /sites/1
+  # GET /sites/1.xml
+  def show
+    @site = Site.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @site }
+    end
+  end
+
+  # GET /sites/new
+  # GET /sites/new.xml
+  def new
+    @site = Site.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @site }
+    end
+  end
+
+  # GET /sites/1/edit
+  def edit
+    @site = Site.find(params[:id])
+  end
+
+  # POST /sites
+  # POST /sites.xml
+  def create
+    @site = Site.new(params[:site])
+
+    respond_to do |format|
+      if @site.save
+        format.html { redirect_to(@site, :notice => 'Site was successfully created.') }
+        format.xml  { render :xml => @site, :status => :created, :location => @site }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @site.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /sites/1
+  # PUT /sites/1.xml
+  def update
+    @site = Site.find(params[:id])
+
+    respond_to do |format|
+      if @site.update_attributes(params[:site])
+        format.html { redirect_to(@site, :notice => 'Site was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @site.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sites/1
+  # DELETE /sites/1.xml
+  def destroy
+    @site = Site.find(params[:id])
+    @site.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(sites_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
