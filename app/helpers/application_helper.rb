4f7c4322db1a90e88c52694a1ab5d485115a4e84
Marcello Henrique <faraohh@gmail.com>
2010-07-14 12:49:08 +0000

    Trabalhando com menus...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,9 +33,9 @@ module ApplicationHelper
       if current_user
         menus += "<div style='float: right;  white-space: nowrap;'>" +
           link_to(image_tag('editar.gif', :border => 0), edit_site_menu_path(params[:site_id], menu)) +
-          link_to(image_tag('subitem.gif', :border => 0), new_site_menu_path(:father_id => menu.id)) +
-          link_to(image_tag('setaup.gif', :border => 0), new_site_menu_path(:father_id => menu.id)) +
-          link_to(image_tag('setadown.gif', :border => 0), new_site_menu_path(:father_id => menu.id)) +
+          link_to(image_tag('subitem.gif', :border => 0), new_site_menu_path(:parent_id => menu.id)) +
+          link_to(image_tag('setaup.gif', :border => 0), new_site_menu_path(:parent_id => menu.id)) +
+          link_to(image_tag('setadown.gif', :border => 0), new_site_menu_path(:parent_id => menu.id)) +
           link_to(image_tag('apagar.gif', :border => 0), site_menu_path(params[:site_id], menu), :confirm => 'are_you_sure', :method => :delete) + "</div>"
       end
       menus += "</div>"
