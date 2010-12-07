ba1e8841d25bd7b8b23d90a6a20c71f388fd416e
Marcello Henrique <faraohh@gmail.com>
2010-12-07 19:36:26 +0000

    Alterando paperclip para salvar arquivos em uploads com id do site.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,15 +1,8 @@
 class Repository < ActiveRecord::Base
-belongs_to :site
+  belongs_to :site
 
-attr_accessor :site_id
-
-
-
-has_attached_file :archive,
-                   :url => "/uploads/:site_id/:class/:attachment/:id/:style_:basename.:extension"
+  has_attached_file :archive, :url => "/uploads/:site_id/:style_:basename.:extension"
                     
-
-#validates_attachment_content_type :mp3, :content_type => [ "image/bmp", "image/x-png", "image/pjpeg", "image/jpg", "image/jpeg", "image/png", "image/gif" ] 
-   validates_attachment_presence :archive
-
+  #validates_attachment_content_type :mp3, :content_type => [ "image/bmp", "image/x-png", "image/pjpeg", "image/jpg", "image/jpeg", "image/png", "image/gif" ] 
+  validates_attachment_presence :archive
 end
