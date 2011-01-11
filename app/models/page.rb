8fb5843afad05ded448cbd846db244ab68e7d5f8
Arthur Henrique <warthur2@gmail.com>
2011-01-11 16:44:04 +0000

    Noticias página principal

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,7 +6,7 @@ class Page < ActiveRecord::Base
   has_many :sites, :through => :sites_pages
 
   has_attached_file :image, 
-     :styles => { :medium => "300X300"},
+     :styles => { :medium => "300X300", :little =>"190X140"},
 	 :url => "/uploads/:class/:site_id/:id/:style_:basename.:extension"	
   
   accepts_nested_attributes_for :sites_pages, :allow_destroy => true#, :reject_if => proc { |attributes| attributes['title'].blank? }
