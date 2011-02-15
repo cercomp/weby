665404312e7d49450fd43bb18e3c267a368c4758
Marcello Henrique <faraohh@gmail.com>
2011-02-15 11:49:07 +0000

    Adicionando spinner quando usar ajax.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,26 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-document.observe("dom:loaded", function() {
-  // the element in which we will observe all clicks and capture
-  // ones originating from pagination links
-  var container = $(document.body)
+$(document).ready(function() {
+  var img = new Image
+  img.src = '/images/spinner.gif'
 
-  if (container) {
-    var img = new Image
-    img.src = '/images/spinner.gif'
-
-    function createSpinner() {
-      return new Element('img', { src: img.src, 'class': 'spinner' })
-    }
-
-    container.observe('click', function(e) {
-      var el = e.element()
-      if (el.match('.pagination.ajax a')) {
-        el.up('.pagination.ajax').insert(createSpinner())
-        new Ajax.Request(el.href, { method: 'get' })
-        e.stop()
-      }
-    })
-  }
+  $('.pagination.ajax a').live('click',function (){
+    $(this).parent().html('').append(img)
+    $.getScript(this.href);
+    return false;
+  })
 })
