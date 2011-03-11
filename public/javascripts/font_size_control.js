946305f8c7bfc7347d9342e72a6ce607d26b1c66
Arthur Henrique <warthur2@gmail.com>
2011-03-11 12:51:18 +0000

    limite tamanho das letras

--- public/javascripts/font_size_control.js
+++ public/javascripts/font_size_control.js
@@ -8,7 +8,9 @@ $(document).ready(function(){
 function font_size_up(){
     var font = $('html').css('font-size'),
     size = parseInt(font.substr(0, font.length - 2)) + 2
-    $('html').css('font-size', size.toString() + 'px') 
+
+    if( size < ( parseInt($.cookie("font_size_original").substr(0, font.length -2)) +10 ) )
+        $('html').css('font-size', size.toString() + 'px') 
 	
 		//atualiza valor de font_size no cookie
 		$.cookie("font_size", size, { path: '/' })
@@ -16,8 +18,10 @@ function font_size_up(){
 
 function font_size_down(){
  	var font = $('html').css('font-size'),
-    size = parseInt(font.substr(0, font.length - 2)) - 2
-    $('html').css('font-size', size.toString() + 'px') 
+    size = parseInt(font.substr(0, font.length - 2)) - 1
+    
+    if( size > ( parseInt($.cookie("font_size_original").substr(0, $.cookie("font_size_original").length -2))-3) )
+        $('html').css('font-size', size.toString() + 'px') 
 
 		//atualiza valor de font_size no cookie
 		$.cookie("font_size", size, { path: '/' })
