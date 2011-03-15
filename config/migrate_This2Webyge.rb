874708d5b524fbeffb9323fbb42260551633d8ab
Marcello Henrique <faraohh@gmail.com>
2011-03-15 20:56:09 +0000

    Trabalhando no arquivo de migração da base de dados This para Weby, parte 7.

--- config/migrate_This2Webyge.rb
+++ config/migrate_This2Webyge.rb
@@ -24,29 +24,34 @@ class Migrate_this2weby
     @con_this = PGconn.connect(@config['this']['host'],nil,nil,nil,@config['this']['database'],@config['this']['username'],@config['this']['password'])
     @con_weby = PGconn.connect(@config['weby']['host'],nil,nil,nil,@config['weby']['database'],@config['weby']['username'],@config['weby']['password'])
     @verbose = verbose
-    @param = "WHERE site_id=17"
+    #@param = "WHERE site_id=17"
     @convar = {} # Variável de conversão
   end
 
   def migrate_this
     puts "Migrando tabela this.sites para weby.sites...\n" if @verbose
-    select_sites = "SELECT * FROM sites #{@param}"
+    select_sites = "SELECT * FROM sites #{@param} ORDER BY site_id"
     puts "\t#{select_sites}\n" if @verbose
     sites_this = @con_this.exec(select_sites)
     # Laço de repetição
     sites_this.each do |s_this|
       site_name = /http:\/\/www.([a-z]+).*\/([a-z]+)/.match("#{s_this['caminho_http']}").nil? ? /http:\/\/www.([a-z]+).*/.match("#{s_this['caminho_http']}")[1] : /http:\/\/www.([a-z]+).*\/([a-z]+)/.match("#{s_this['caminho_http']}")[2]
+      site_name ||= s_this['site_id']
       select_rodape = "SELECT * FROM rodape WHERE site_id='#{s_this['site_id']}'"
+      puts "\tRodapé: #{select_rodape}"
       rodape = @con_this.exec(select_rodape)
-      insert_site = "INSERT INTO sites (name,url,description,footer) VALUES ('#{site_name}','#{s_this['caminho_http']}','#{treatment(s_this['nm_site'])}','#{rodape[0]['endereco']} #{rodape[0]['telefone']}') RETURNING id"
+      #rodape_text = "#{rodape[0]['endereco']} #{rodape[0]['telefone']}" if not rodape[0].nil?
+      rodape_text = ""
+      insert_site = "INSERT INTO sites (name,url,description,footer) VALUES ('#{site_name}','#{s_this['caminho_http']}','#{pre_treat(s_this['nm_site'])}','#{rodape_text}') RETURNING id"
       site = @con_weby.exec(insert_site)
       puts "\t\t(#{site[0]['id']}) #{insert_site}\n" if @verbose
       # Criando objeto de conversão
       @convar["#{s_this['site_id']}"] = {}
       @convar["#{s_this['site_id']}"]["weby"] = site[0]['id']
+      @convar["#{s_this['site_id']}"]["weby_name"] = "#{site_name}"
 
       puts "Migrando tabela this.usuarios para weby.users...\n" if @verbose
-      select_usuarios = "SELECT * FROM usuarios #{@param}"
+      select_usuarios = "SELECT * FROM usuarios #{@param} WHERE site_id='#{s_this['site_id']}' ORDER BY id"
       puts "\t#{select_usuarios}\n" if @verbose
       users_this = @con_this.exec(select_usuarios)
       # Criando objeto de conversão
@@ -68,14 +73,18 @@ class Migrate_this2weby
       # Migrando Tabela: this.[noticias,eventos,informativos] => weby.pages
       # Criando objeto de conversão
       @convar["#{s_this['site_id']}"]["noticias"] = {}
-      select_noticias = "SELECT * FROM noticias WHERE site_id='#{s_this['site_id']}'"
+      select_noticias = "SELECT * FROM noticias WHERE site_id='#{s_this['site_id']}' ORDER BY id"
       puts "\t\t\t#{select_noticias}\n" if @verbose
       this_noticias = @con_this.exec(select_noticias)
       this_noticias.each do |t_n|
         capa = t_n['capa'] != false ? true : false
-        dt_fim = t_n['dt_fim'].nil? ? 2.years.from_now : t_n['dt_fim']
+        dt_cadastro = ((t_n['dt_cadastro'].nil?) || (/([-]+)/.match("#{t_n['dt_cadastro']}").nil?)) ? Time.now : t_n['dt_cadastro']
+        dt_inicio = ((t_n['dt_inicio'].nil?) || (/([-]+)/.match("#{t_n['dt_inicio']}").nil?)) ? Time.now : t_n['dt_inicio']
+        dt_fim = ((t_n['dt_fim'].nil?) || (/([-]+)/.match("#{t_n['dt_fim']}").nil?)) ? 2.years.from_now : t_n['dt_fim']
         status = t_n['status'] == 'P' ? true : false
-        insert_pages = "INSERT INTO pages (created_at,updated_at,date_begin_at,date_end_at,site_id,author_id,text,url,source,title,summary,publish,front,type) VALUES ('#{t_n['dt_cadastro']}','#{t_n['dt_cadastro']}','#{t_n['dt_inicio']}','#{dt_fim}','#{site[0]['id']}','#{@convar["#{s_this['site_id']}"]['usuarios'][t_n['autor']]}','#{treatment(t_n['texto'])}','#{treatment(t_n['url'])}','#{treatment(t_n['fonte'])}','#{treatment(t_n['titulo'])}','#{treatment(t_n['resumo'])}',#{status},#{capa},'News') RETURNING id"
+        autor = @convar["#{s_this['site_id']}"]['usuarios'][t_n['autor']]
+        autor ||= 1
+        insert_pages = "INSERT INTO pages (created_at,updated_at,date_begin_at,date_end_at,site_id,author_id,text,url,source,title,summary,publish,front,type) VALUES ('#{dt_cadastro}','#{dt_cadastro}','#{dt_inicio}','#{dt_fim}','#{site[0]['id']}','#{autor}','#{pre_treat(t_n['texto'])}','#{pre_treat(t_n['url'])}','#{pre_treat(t_n['fonte'])}','#{pre_treat(t_n['titulo'])}','#{pre_treat(t_n['resumo'])}',#{status},#{capa},'News') RETURNING id"
         page = @con_weby.exec(insert_pages)
         puts "\t\t\t\t(#{page[0]['id']}) #{insert_pages[0,300]}\n" if @verbose
         insert_sites_pages = "INSERT INTO sites_pages (site_id,page_id) VALUES ('#{site[0]['id']}','#{page[0]['id']}')"
@@ -92,7 +101,9 @@ class Migrate_this2weby
       this_paginas = @con_this.exec(select_paginas)
       this_paginas.each do |t_n|
         data_publica = ((t_n['dt_publica'].nil?) || (/([-]+)/.match("#{t_n['dt_publica']}").nil?)) ? 2.years.from_now : t_n['dt_publica']
-        insert_pages = "INSERT INTO pages (created_at,date_begin_at,date_end_at,site_id,author_id,title,text,publish,front,type) VALUES ('#{Time.now}','#{Time.now}','#{data_publica}','#{site[0]['id']}','#{@convar["#{s_this['site_id']}"]['usuarios'][t_n['autor']]}','#{treatment(t_n['titulo'])}','#{treatment(t_n['texto'])}',true,false,'News') RETURNING id"
+        autor = @convar["#{s_this['site_id']}"]['usuarios'][t_n['autor']]
+        autor ||= 1
+        insert_pages = "INSERT INTO pages (created_at,date_begin_at,date_end_at,site_id,author_id,title,text,publish,front,type) VALUES ('#{Time.now}','#{Time.now}','#{data_publica}','#{site[0]['id']}','#{autor}','#{pre_treat(t_n['titulo'])}','#{pre_treat(t_n['texto'])}',true,false,'News') RETURNING id"
         page = @con_weby.exec(insert_pages)
         puts "\t\t\t\t(#{page[0]['id']}) #{insert_pages[0,300]}\n" if @verbose
         insert_sites_pages = "INSERT INTO sites_pages (site_id,page_id) VALUES ('#{site[0]['id']}','#{page[0]['id']}')"
@@ -112,9 +123,15 @@ class Migrate_this2weby
       this_eventos.each do |t_n|
         capa = t_n['capa'] != false ? true : false
         tipo = t_n['tipo'].nil? ? '' : kind_list["#{t_n['tipo']}"]
-        dt_fim = t_n['dt_fim'].nil? ? 2.years.from_now : t_n['dt_fim']
+        dt_cadastro = ((t_n['dt_cadastro'].nil?) || (/([-]+)/.match("#{t_n['dt_cadastro']}").nil?)) ? Time.now : t_n['dt_cadastro']
+        dt_inicio = ((t_n['dt_inicio'].nil?) || (/([-]+)/.match("#{t_n['dt_inicio']}").nil?)) ? Time.now : t_n['dt_inicio']
+        dt_fim = ((t_n['dt_fim'].nil?) || (/([-]+)/.match("#{t_n['dt_fim']}").nil?)) ? 2.years.from_now : t_n['dt_fim']
+        inicio = ((t_n['inicio'].nil?) || (/([-]+)/.match("#{t_n['inicio']}").nil?)) ? Time.now : t_n['inicio']
+        fim = ((t_n['fim'].nil?) || (/([-]+)/.match("#{t_n['fim']}").nil?)) ? Time.now : t_n['fim']
         status = t_n['status'] == 'P' ? true : false
-        insert_pages = "INSERT INTO pages (created_at,updated_at,date_begin_at,date_end_at,event_begin,event_end,site_id,author_id,text,url,source,title,summary,publish,front,type,kind,event_email,local) VALUES ('#{t_n['dt_cadastro']}','#{t_n['dt_cadastro']}','#{t_n['dt_inicio']}','#{dt_fim}','#{t_n['inicio']}','#{t_n['fim']}','#{site[0]['id']}','#{@convar["#{s_this['site_id']}"]['usuarios'][t_n['autor']]}','#{treatment(t_n['texto'])}','#{treatment(t_n['url'])}','#{treatment(t_n['fonte'])}','#{treatment(t_n['titulo'])}','#{treatment(t_n['resumo'])}',#{status},#{capa},'Event','#{tipo}','#{t_n['email']}','#{t_n['local_realiza']}') RETURNING id"
+        autor = @convar["#{s_this['site_id']}"]['usuarios'][t_n['autor']]
+        autor ||= 1
+        insert_pages = "INSERT INTO pages (created_at,updated_at,date_begin_at,date_end_at,event_begin,event_end,site_id,author_id,text,url,source,title,summary,publish,front,type,kind,event_email,local) VALUES ('#{dt_cadastro}','#{dt_cadastro}','#{dt_inicio}','#{dt_fim}','#{inicio}','#{fim}','#{site[0]['id']}','#{autor}','#{pre_treat(t_n['texto'])}','#{pre_treat(t_n['url'])}','#{pre_treat(t_n['fonte'])}','#{pre_treat(t_n['titulo'])}','#{pre_treat(t_n['resumo'])}',#{status},#{capa},'Event','#{tipo}','#{t_n['email']}','#{pre_treat(t_n['local_realiza'])}') RETURNING id"
         page = @con_weby.exec(insert_pages)
         puts "\t\t\t\t(#{page[0]['id']}) #{insert_pages[0,300]}\n" if @verbose
         insert_sites_pages = "INSERT INTO sites_pages (site_id,page_id) VALUES ('#{site[0]['id']}','#{page[0]['id']}')"
@@ -131,9 +148,13 @@ class Migrate_this2weby
       this_informativos = @con_this.exec(select_informativos)
       # Conversão de tipo de evento
       this_informativos.each do |t_n|
-        dt_fim = t_n['dt_fim'].nil? ? 2.years.from_now : t_n['dt_fim']
+        dt_cadastro = ((t_n['dt_cadastro'].nil?) || (/([-]+)/.match("#{t_n['dt_cadastro']}").nil?)) ? Time.now : t_n['dt_cadastro']
+        dt_inicio = ((t_n['dt_inicio'].nil?) || (/([-]+)/.match("#{t_n['dt_inicio']}").nil?)) ? Time.now : t_n['dt_inicio']
+        dt_fim = ((t_n['dt_fim'].nil?) || (/([-]+)/.match("#{t_n['dt_fim']}").nil?)) ? 2.years.from_now : t_n['dt_fim']
+        autor = @convar["#{s_this['site_id']}"]['usuarios'][t_n['autor']]
+        autor ||= 1
         status = t_n['status'] == 'P' ? true : false
-        insert_banner = "INSERT INTO banners (created_at,updated_at,date_begin_at,date_end_at,site_id,user_id,text,url,title,publish,hide) VALUES ('#{t_n['dt_cadastro']}','#{t_n['dt_cadastro']}','#{t_n['dt_inicio']}','#{dt_fim}','#{site[0]['id']}','#{@convar["#{s_this['site_id']}"]['usuarios'][t_n['autor']]}','#{treatment(t_n['texto'])}','#{treatment(t_n['url'])}','#{treatment(t_n['assunto'])}',#{status},false) RETURNING id"
+        insert_banner = "INSERT INTO banners (created_at,updated_at,date_begin_at,date_end_at,site_id,user_id,text,url,title,publish,hide) VALUES ('#{dt_cadastro}','#{dt_cadastro}','#{dt_inicio}','#{dt_fim}','#{site[0]['id']}','#{autor}','#{pre_treat(t_n['texto'])}','#{pre_treat(t_n['url'])}','#{pre_treat(t_n['assunto'])}',#{status},false) RETURNING id"
         banner = @con_weby.exec(insert_banner)
         puts "\t\t\t\t(#{banner[0]['id']}) #{insert_banner[0,300]}\n" if @verbose
         # Relacionando notícias na variável de conversão
@@ -150,21 +171,35 @@ class Migrate_this2weby
 
     end
     sites_this.clear()
+
+    # Tratando links de weby.pages
+    select_pages = "SELECT id,title,url,source,summary,text FROM pages"
+    puts "\t\t\t#{select_pages}\n" if @verbose
+    weby_pages = @con_weby.exec(select_pages)
+    weby_pages.each do |weby_page|
+      update_page = "UPDATE pages SET title='#{treat(weby_page['title'])}',url='#{treat(weby_page['url'])}',source='#{treat(weby_page['source'])}',summary='#{treat(weby_page['summary'])}',text='#{treat(weby_page['text'])}' WHERE id='#{weby_page['id']}'"
+      puts "\t\t\t\t(#{weby_page['id']}) #{update_page[0,300]}\n" if @verbose
+      page = @con_weby.exec(update_page)
+    end
+    weby_pages.clear()
+
   end
 
   # Metodo para chamada recursiva
   def deep_insert_menu(sons, entry, this_id, site_id, menu_id, type)
-    if not entry['texto'].empty? # Se o campo texto for significante o menu está embutido
-      insert_page = "INSERT INTO pages (created_at,date_begin_at,date_end_at,site_id,author_id,title,text,publish,front,type) VALUES ('#{Time.now}','#{Time.now}','#{Time.now}','#{site_id}','#{@convar["#{this_id}"]['usuarios'][entry['modificador']]}','#{treatment(entry['texto_item'])}','#{treatment(entry['texto'])}',true,false,'News') RETURNING id"
+    if not entry['texto'].nil? # Se o campo texto for significante o menu está embutido
+      modificador = @convar["#{this_id}"]['usuarios'][entry['modificador']] 
+      modificador ||= 1
+      insert_page = "INSERT INTO pages (created_at,date_begin_at,date_end_at,site_id,author_id,title,text,publish,front,type) VALUES ('#{Time.now}','#{Time.now}','#{Time.now}','#{site_id}','#{modificador}','#{pre_treat(entry['texto_item'])}','#{pre_treat(entry['texto'])}',true,false,'News') RETURNING id"
       page_id = @con_weby.exec(insert_page)
       puts "\t\t\t\t\t(#{page_id[0]['id']}) #{insert_page[0,300]}" if @verbose
-      insert_menu = "INSERT INTO menus (title,link,page_id) VALUES ('#{treatment(entry['texto_item'])}','','#{page_id[0]['id']}') RETURNING id"
+      insert_menu = "INSERT INTO menus (title,link,page_id) VALUES ('#{pre_treat(entry['texto_item'])}','','#{page_id[0]['id']}') RETURNING id"
     elsif not /javascript:mostrar_pagina.*\('([0-9]+)'.*/.match("#{entry['url']}").nil? # Verificando se o menu é interno, externo
       page_id = /javascript:mostrar_pagina.*\('([0-9]+)'.*/.match("#{entry['url']}")[1]
       page_id = @convar["#{this_id}"]["paginas"]["#{page_id}"]
-      insert_menu = "INSERT INTO menus (title,link,page_id) VALUES ('#{treatment(entry['texto_item'])}','','#{page_id}') RETURNING id"
+      insert_menu = "INSERT INTO menus (title,link,page_id) VALUES ('#{pre_treat(entry['texto_item'])}','','#{page_id}') RETURNING id"
     else
-      insert_menu = "INSERT INTO menus (title,link) VALUES ('#{treatment(entry['texto_item'])}','#{treatment(entry['url'])}') RETURNING id"
+      insert_menu = "INSERT INTO menus (title,link) VALUES ('#{pre_treat(entry['texto_item'])}','#{pre_treat(entry['url'])}') RETURNING id"
     end
     menu_sub = @con_weby.exec(insert_menu)
     puts "\t\t\t\t(#{menu_sub[0]['id']}) #{insert_menu}\n" if @verbose
@@ -198,10 +233,25 @@ class Migrate_this2weby
     end
   end
 
-  def treatment(string)
-    coder = HTMLEntities.new
-    string = coder.decode(string)
-    string = @con_weby.escape(string)
+  def pre_treat(string)
+    unless string.nil?
+      coder = HTMLEntities.new
+      string = coder.decode(string)
+      string = @con_weby.escape(string)
+    end
+    return string
+  end
+ 
+  def treat(string)
+    unless string.nil?
+      if string.match(/javascript:mostrar_pagina\('([0-9]+)','([0-9]+)'\);/)
+        string.gsub!(/javascript:mostrar_pagina\('([0-9]+)','([0-9]+)'\);/){|x| "/sites/#{@convar[$2]['weby_name']}/pages/#{@convar[$2]["paginas"][$1]}" }
+      end
+      if string.match(/javascript:mostrar_noticia\('([0-9]+)','([0-9]+)'\);/)
+        string.gsub!(/javascript:mostrar_noticia\('([0-9]+)','([0-9]+)'\);/){|x| "/sites/#{@convar[$2]['weby_name']}/pages/#{@convar[$2]["noticias"][$1]}" }
+      end
+      string = @con_weby.escape(string)
+    end
     return string
   end
 
