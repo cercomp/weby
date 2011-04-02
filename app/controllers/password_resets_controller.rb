4dddf610876804032b1ebeea7b6f07f1167fba57
Marcello Henrique <faraohh@gmail.com>
2011-04-02 01:45:58 +0000

    Variável passada para tinymce depende do templates erb. Correção recuperar senha.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,12 +1,12 @@
 # coding: utf-8
 class PasswordResetsController < ApplicationController
   layout :choose_layout
-  before_filter :load_user_using_perishable_token, :only => [:edit, :update]  
-  before_filter :require_no_user  
+  before_filter :load_user_using_perishable_token, :only => [:edit, :update]
+  before_filter :require_no_user
   skip_before_filter :check_authorization
 
   def new
-    flash[:warning] = t"fill_email_form"
+    flash.now[:warning] = t"fill_email_form"
   end
 
   def create
@@ -21,26 +21,26 @@ class PasswordResetsController < ApplicationController
     end
   end
   
-  def edit  
+  def edit
   end  
   
   def update
-    @user.password = params[:user][:password]  
-    @user.password_confirmation = params[:user][:password_confirmation]  
-    if @user.save  
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.save
       flash[:notice] = t"successfully_updated", :param => t("password")
       redirect_to @user
-    else  
-      render :action => :edit  
-    end  
-  end  
+    else
+      render :action => :edit
+    end
+  end
   
-  private  
-  def load_user_using_perishable_token  
-    @user = User.find_using_perishable_token(params[:id])  
-    unless @user  
+  private
+  def load_user_using_perishable_token
+    @user = User.find_using_perishable_token(params[:id])
+    unless @user
       flash[:error] = t("missing_account")
       redirect_to root_url
-    end  
-  end  
+    end
+  end
 end
