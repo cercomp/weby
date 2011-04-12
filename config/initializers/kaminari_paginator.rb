b96fbb67f97446bfbf5ec0de7918d34039b442de
Marcello Henrique <faraohh@gmail.com>
2011-04-12 13:51:38 +0000

    Correção inicializador do kaminari.

--- config/initializers/kaminari_paginator.rb
+++ config/initializers/kaminari_paginator.rb
@@ -1,5 +1,5 @@
 module Kaminari
-  if Site.columns_hash['per_page']
+  if Site.table_exists? and Site.columns_hash['per_page']
     DEFAULT_PER_PAGE = Site.columns_hash['per_page'].default.gsub(/[^\d,]/,'').split(',').first.to_i
   end
 end
