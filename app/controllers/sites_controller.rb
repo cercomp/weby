8e6aae449a464cbfde6fa94c0e2418f81025f800
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-04-25 15:01:31 +0000

    corrigindo ordenação de tabelas para funcionar com paginação

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -8,6 +8,7 @@ class SitesController < ApplicationController
 
   def index
     @sites = Site.name_or_description_like(params[:search]).
+      except(:order).
       order(sort_column + " " + sort_direction). # FIXME Ordenação não está funcionando
       page(params[:page]).
       per(params[:per_page])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -281,9 +281,7 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    params[:sort] = column
-    params[:direction] = direction
-    link_to title, params, {:class => css_class}
+    link_to title, params.merge({:sort => column, :direction => direction}), {:class => css_class}
   end
 
   # Informações sobre paginação
