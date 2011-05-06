93e81689233d19c47ee1816666414daa7b7dd157
Danilo Inacio <danilo.isr@gmail.com>
2011-05-06 18:58:01 +0000

    trabalhando no layout do weby

--- app/TODO.rb
+++ app/TODO.rb
@@ -1,1 +1,5 @@
 # FIXME: Script de migração não está tratando javascript:mostrar_menu(...)
+
+#TODO: paginação no layout do weby está quebrando o layout
+
+#TODO: layout do weby não está funcionando no IE8
