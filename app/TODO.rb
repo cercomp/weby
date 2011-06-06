b1fed0da424f1f0a66af4853f75e98f4feb29e4c
Arthur Henrique <warthur2@gmail.com>
2011-06-06 18:00:23 +0000

    adicionado TODO referente a listagem dos menus

--- app/TODO.rb
+++ app/TODO.rb
@@ -1,2 +1,3 @@
+#TODO: Melhorar apresentação do index dos menus -> Substituir setas pelo drag-and-drop, substituir lápis por clicar no nome do menu.
 #TODO: paginação no layout do weby está quebrando o layout
 #TODO: layout do weby não está funcionando no IE8
