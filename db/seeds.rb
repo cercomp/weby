32124a672eff23beaf046995abefa8b3d3e5b8c1
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-06-22 14:56:55 +0000

    melhorando aparencia do menu weby_bar para combinar qualquer cor de fundo dos sites.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,18 @@ end
 load(Rails.root.join('db', 'seed', 'default_per_page.rb').to_s)
 load(Rails.root.join('db', 'seed', 'default_settings.rb').to_s)
 #Fixtures.create_fixtures("#{Rails.root}/db/seed", "*.yml")  
+
+# Permissões Globais
+Role.create :name => 'Gerente',
+  :right_ids => ["26", "23", "24", "25", "27", "22", "47", "45", "46", "43",
+                 "48", "49", "44", "32", "30", "31", "28", "29", "21", "18",
+                 "19", "16", "20", "17", "14", "11", "12", "9", "13", "15",
+                 "10", "42", "40", "41", "38", "39", "6", "5"]
+
+Role.create :name => 'Editor-Chefe',
+  :right_ids => ["26", "23", "24", "25", "27", "22", "21", "18", "19", "16",
+                 "20", "17", "14", "11", "12", "9", "13", "15", "10", "42",
+                 "40", "38", "39"]
+
+Role.create :name => 'Redator',
+  :right_ids => ["14", "11", "9", "10"]
