02aacd5349e78094eccddda8e4ecca9f89001040
Danilo Inacio <danilo.isr@gmail.com>
2011-08-23 20:48:50 +0000

    Adicionado valor 'default_locale' nas configurações do site

--- db/seed/default_settings.rb
+++
@@ -1,2 +0,0 @@
-Setting.create :name => 'per_page', :value => '5,15,30,60,100'
-Setting.create :name => 'per_page_default', :value => '25'
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ User.create do |u|
 end
 
 load(Rails.root.join('db', 'seed', 'default_per_page.rb').to_s)
-load(Rails.root.join('db', 'seed', 'default_settings.rb').to_s)
+#load(Rails.root.join('db', 'seed', 'default_settings.rb').to_s)
 #Fixtures.create_fixtures("#{Rails.root}/db/seed", "*.yml")  
 
 # Permissões Globais
