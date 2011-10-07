447b9bfd5694201bcf1e5657e56f36996ffd1e7a
Marcello Henrique <faraohh@gmail.com>
2011-10-07 17:36:29 +0000

    Definindo this como leiaute padrão e redirecionando para os componentes na criação de um site.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,7 +2,7 @@ class SitesController < ApplicationController
   layout :choose_layout
   before_filter :require_user, :only => [:new, :create, :edit, :update, :destroy]
   before_filter :check_authorization, :except => [:show, :index]
-  respond_to :html, :xml
+  respond_to :html, :xml, :js
 
   helper_method :sort_column
 
@@ -31,19 +31,17 @@ class SitesController < ApplicationController
     (Dir[File.join(Rails.root + "app/views/layouts/[a-zA-Z]*.erb")] - Dir[File.join(Rails.root + "app/views/layouts/portal.html.erb")]).each do |file|
       @themes << file.split("/")[-1].split(".")[0]
     end
-    respond_with(@site)
   end
 
   def edit
     @repositories = Repository.search(params[:search], params[:page],["archive_content_type LIKE ?","image%"])
-    #    @repositories = Repository.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 1 
     @site = Site.find_by_name(params[:id])
   end
 
   def create
     @site = Site.new(params[:site])
     if @site.save
-      redirect_to site_admin_path(@site)
+      redirect_to site_site_components_path(@site)
     else
       respond_with @site
     end
