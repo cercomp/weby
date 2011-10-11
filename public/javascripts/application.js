18c49cde072f868b5063436359ea82c523fea0a7
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-10-11 15:28:29 +0000

    otimizando, padronizando e centralizando o loading para ficar sempre igual em qualquer tela, apenas de colocar data-remote=true o sistema já carrega o loading

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,30 +1,27 @@
 $(document).ready(function() {
-  var loading = LoadingGif.build();
-
+  // Ajax indicator
   $('form[data-remote=true]').bind('submit', function(){
-    loading.setLocal($(this).find('span#loading')).show();
-  }).bind('ajax:complete', function(){
-    loading.hide();
+    Loading.show();
+  }).ajaxComplete(function(){
+    Loading.hide();
   });
 
-  // Mostra spinner em ajax com pagination
-  $('.pages_paginator a[data-remote=true]').live('click',function (){
-    loading.setLocal($(this).parent().parent()).show();
+  $('a[data-remote=true]').live('click',function (){
+    Loading.show();
     return false;
-  })
+  }).ajaxComplete(function(){
+    Loading.hide();
+    return false;
+  });
 
-  // Mostra spinner em ajax com links de itens por pagina
-  $('.per_page_paginator a[data-remote=true]').live('click',function (){
-    loading.setLocal($(this).parent().parent()).show();
-    return false;
-  })
+  /////////////////////////////////////////////////////////////////////////////
 
   // ManageRoles limpa area do formulário
   $('.role_edit').each(function (link) {
     $(this).bind("ajax:success", function(data, status, xhr) {
-      $('#user_'+$(this).attr('user_id')).hide()
-    })
-  })
+      $('#user_'+$(this).attr('user_id')).hide();
+    });
+  });
 
   // ManageRoles muda o cursor do ponteiro
   $('.role_edit').each( function (link) {
@@ -58,26 +55,23 @@ function addToSelect(selectId){
 }
 
 // Objeto para carregar gif
-var LoadingGif = {
-  build: function(obj){
-    this.obj = obj;
-    this.img = new Image();
-    this.img.src = '/images/spinner.gif';
-    return this;
-  },
-  setLocal: function(obj){
-    this.obj = obj;
-    return this;
-  },
+Loading = {
+  place_holder: $(document.createElement('div')), 
+  image: new Image(),
   show: function(){
-    this.obj.html(this.img).show();
-    return this;
+    if($('#ajax-indicator').length <= 0){
+      this.place_holder.attr('id', 'ajax-indicator');
+      this.place_holder.addClass('ajax-indicator');
+      this.image.src = '/images/loading.gif';
+      this.place_holder.html(this.image).append(' Loading...');
+      this.place_holder.appendTo($('body'));
+    }
+    this.place_holder.show();
   },
   hide: function(){
-    this.obj.html('').hide();
-    return this;
+    this.place_holder.hide();
   }
-};
+}
 
 function toogle_select_multiple(select){
   if($(select).attr("multiple")){
