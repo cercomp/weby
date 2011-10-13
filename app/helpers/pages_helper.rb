3ece2c91adb001318da5245f63d56dbead830530
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-10-13 18:02:01 +0000

    corrigindo para mostrar svg no page#show

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -8,11 +8,15 @@ module PagesHelper
   end
 
   def link_to_cover_image(page)
-  link_to image_tag("#{page.repository.archive.url(:mini)}",
-                    :alt => page.repository.description),
-                    site_page_path(@site, page, :type => params[:type]),
-                    :title => page.repository.description
+    link_to image_tag("#{page.repository.archive.url(:mini)}",
+                      :alt => page.repository.description),
+                      site_page_path(@site, page, :type => params[:type]),
+                      :title => page.repository.description
   end  
+
+  def thumbnail_on_show
+    raw image_tag(page_image, align: "right", size: page_image_size)
+  end
 
   # Retorna um link externo quando existente ou um link interno da página. 
   # Recebe uma página e o site.
@@ -22,6 +26,21 @@ module PagesHelper
       else
         page.url
       end
+  end
+
+  private
+  def page_image
+    page_image? ? 
+      @page.repository.archive.url(:little) :
+      @page.repository.archive.url(:original)
+  end
+
+  def page_image_size
+    '128x128' unless page_image?
+  end
+
+  def page_image?
+    @page.repository.image?
   end
 
 end
