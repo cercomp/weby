44e42534a4de302bc24433043021a8911be0f0f6
Ricardo Silva <ricardops.inf@gmail.com>
2011-12-05 11:44:15 +0000

    Permitindo data de publicação infinita e criando helper date para axiliar manipulação de datas em Banners(_list.html.erb) e Páginas(_list.html.rb). Resolve #540

---
+++ app/helpers/date_helper.rb
@@ -0,0 +1,17 @@
+module DateHelper
+  def print_date_end(date)
+    return  t("infinite") if date.nil?
+    return date 
+  end
+
+  #Recebe um objeto page ou banner e verifica se a data de publicação está expirada ou não 
+  def publication_status(obj) 
+    return raw toggle_field(obj, "publish") if obj.date_end_at.nil?
+    return raw toggle_field(obj, "publish") if publish_date?(obj) 
+    return t("expired") 
+  end
+
+  def publish_date?(obj)
+    obj.date_begin_at <= Time.now and obj.date_end_at > Time.now
+  end
+end
