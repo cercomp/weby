b99449603798953a2c34ec4582d3f4f2484a3027
Arthur Henrique <warthur2@gmail.com>
2011-12-22 17:56:47 +0000

    padronizando icones

--- app/helpers/page_i18ns_helper.rb
+++ app/helpers/page_i18ns_helper.rb
@@ -5,9 +5,9 @@ module PageI18nsHelper
   def link_to_add_page_i18n(page, site)
     if check_permission(PagesController, ['edit'] )
       if page.page_i18ns.size < Locale.all.size #verifica se todos os locales já foram utilizados
-        link_to (t('i18n')), add_i18n_site_page_path(site, page), :class => 'icon-i18n'
+        link_to (t('i18n')), add_i18n_site_page_path(site, page), :class => 'icon icon-i18n'
       else 
-        image_tag("forms/i18n_off.png", :alt => t("no_more_i18ns")) + t('i18n')
+        image_tag("menus/i18n_off.png", :alt => t("no_more_i18ns")) + t('i18n')
       end
     end
   end
