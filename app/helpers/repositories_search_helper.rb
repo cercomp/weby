e0464fee8a9aaac24802ddbd6ae1f590406dedf1
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-12-28 20:53:05 +0000

    corrigindo commits anteriores, tarefa #574

--- app/helpers/repositories_search_helper.rb
+++ app/helpers/repositories_search_helper.rb
@@ -4,4 +4,12 @@ module RepositoriesSearchHelper
                      id: "#{label_value}_#{file.id}" })
     check_box_tag(name, file.id, true, options)
   end
+
+  def file_selector(many, name, value = "1", checked = false, options = {})
+    if many.to_s == 'true'
+      check_box_tag(name, value, checked, options)
+    else
+      radio_button_tag(name, value, checked, options)
+    end
+  end
 end
