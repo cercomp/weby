20431a5fddc73410553776587026dad91b37746c
Nícolas Lazarte <nicolaslazartekaqui@gmail.com>
2011-12-30 14:23:45 +0000

    Tirando o Jquery Ui do menu#new #589

--- app/helpers/banners_helper.rb
+++ app/helpers/banners_helper.rb
@@ -9,18 +9,18 @@ module BannersHelper
 
   def link_to_banner_type(type, action)
     link_to t(type.to_s), {controller: 'banners', action: action.to_s, type: type.to_s},
-      class: "button",
+      class: "box link",
       update: "div_link",
       remote: true
   end
 
   def links_to_banner_type(type = "internal", action)
     links_hash = {
-      'internal' => "<span class='button'>#{t('internal')}</span>#{link_to_banner_type(:external, action)}",
-      'external' => "#{link_to_banner_type(:internal, action)}<span class='button'>#{t('external')}</span>"
+      'internal' => "<span class='box link'>#{t('internal')}</span>#{link_to_banner_type(:external, action)}",
+      'external' => "#{link_to_banner_type(:internal, action)}<span class='box link'>#{t('external')}</span>"
     }
-
-    raw links_hash[type]
+    
+		raw links_hash[type]
   end
 end
 
