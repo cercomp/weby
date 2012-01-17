1580a0e9e7982c11dac414e8800dbdd617b52494
Marcello Henrique <faraohh@gmail.com>
2012-01-17 11:52:51 +0000

    Pesquisar também pelo campo de email dos usuários.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :groups
 
   scope :login_or_name_like, lambda { |text|
-    where('LOWER(login) like :text OR LOWER(first_name) like :text OR LOWER(last_name) like :text',
+    where('LOWER(login) like :text OR LOWER(first_name) like :text OR LOWER(last_name) like :text OR LOWER(email) like :text',
           { :text => "%#{text.try(:downcase)}%" })
   }
 
