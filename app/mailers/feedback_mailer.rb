acf0859a0e7d4eae43f23f0feb1a3614fa1a1667
Marcello Henrique <faraohh@gmail.com>
2012-02-18 02:18:17 +0000

    Corrigindo campo 'from' do fale conosco.

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,8 +2,7 @@ class FeedbackMailer < ActionMailer::Base
 
   def send_feedback (feedback,destination)
     @feedback = feedback
-    from = "web@cercomp.ufg.br"
-    mail(:from => from, :to => destination, :subject => feedback.subject)
+    mail(:from => feedback.email, :to => destination, :subject => feedback.subject)
   end
 
 end
