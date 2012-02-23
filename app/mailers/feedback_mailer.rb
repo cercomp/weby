20a1c4ee272d34e47184fbcb6d89a63b3bc3756d
Leonardo Souza <porkinhu@gmail.com>
2012-02-23 12:24:06 +0000

    setar o "responder para" com o email do feedback

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,7 +2,7 @@ class FeedbackMailer < ActionMailer::Base
 
   def send_feedback (feedback,destination)
     @feedback = feedback
-    mail(:from => feedback.email, :to => destination, :subject => feedback.subject)
+    mail(:from => feedback.email, :reply_to => feedback.mail, :to => destination, :subject => feedback.subject)
   end
 
 end
