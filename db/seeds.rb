ae4640d8d8f3e2c7c04fb83a84df5094f1b61102
Marcello Henrique <faraohh@gmail.com>
2012-02-27 16:51:22 +0000

    Alterando senha padrão.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ end
 
 User.create do |u|
   u.login = 'admin'
-  u.password = u.password_confirmation = 'admin'
+  u.password = u.password_confirmation = 'Admin1'
   u.email = 'admin@domain.com'
   u.first_name = 'Administrador'
   u.last_name = 'do Sistema'
