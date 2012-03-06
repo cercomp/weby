057662d824ae231fe761cebf8ba371fd5e7cfad2
leo-souza <porkinhu@gmail.com>
2012-03-06 10:50:05 -0300

    bug na criação de pagina, no campo position

--- app/models/site_component.rb
+++ app/models/site_component.rb
@@ -1,4 +1,8 @@
 class SiteComponent < ActiveRecord::Base
   before_create { self.publish ||= true }
   belongs_to :site
+
+  def self.by_setting(setting, value)
+    self.where("settings LIKE '%:#{setting} => \"#{value}\"%'")
+  end
 end
