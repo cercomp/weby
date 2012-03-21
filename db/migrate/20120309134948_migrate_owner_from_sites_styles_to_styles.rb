7fcb527388355246ae10eb1cbfad8b410e9c27fe
Nícolas Lazarte Kaqui <nicolaslazartekaqui@gmail.com>
2012-03-20 22:36:00 -0300

    fixed bug in migrates

--- db/migrate/20120309134948_migrate_owner_from_sites_styles_to_styles.rb
+++ db/migrate/20120309134948_migrate_owner_from_sites_styles_to_styles.rb
@@ -2,32 +2,26 @@ class MigrateOwnerFromSitesStylesToStyles < ActiveRecord::Migration
   def up
     SitesStyle.all.each do |site_style|
       if site_style.owner == true
-        if site_style.style.owner_id.nil?
-          site_style.style.owner_id = site_style.site.id
-          site_style.style.save
+        style = Style.find(site_style.style_id)
+        if style.owner_id.nil?
+          style.update_attributes(owner_id: site_style.site_id)
         else 
-          style = site_style.style.dup
-          style.owner_id = site_style.site.id
-          site_style.style = style
-          site_style.style.save
+          newstyle = Style.new(name: style.name, css: style.css, owner_id: site_style.site_id)
+          newstyle.save
+          site_style.update_attributes(style_id: newstyle.id)
         end
       end
-      site_style.owner = nil
-      site_style.save
     end
   end
 
   def down
-    SitesStyle.all.each do |site_style|
-      if site_style.style.owner_id == site_style.site_id
-        site_style.style.owner_id = nil
-        site_style.style.save
-        site_style.owner = true
-        site_style.save
-      else 
-        site_style.owner = false
-        site_style.save
-      end
+    Style.all.each do |style|
+      site_style = SitesStyle.where(style_id: style.id, site_id: style.owner_id).first
+      site_style.update_attributes(owner: true)
+      style.update_attributes(owner_id: nil)
+    end
+    SitesStyle.where(owner: nil).each do |site_style|
+      site_style.update_attributes(owner: false)
     end
   end
 end
