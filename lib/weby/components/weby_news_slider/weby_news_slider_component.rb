450963fa8aa6a6920e17ad41dfbd58cd499a91f3
Danilo Resende <danilo.isr@gmail.com>
2012-05-29 00:24:53 -0300

    Componente de slider de noticias aceita opção de nenhuma categoria

--- lib/weby/components/weby_news_slider/weby_news_slider_component.rb
+++ lib/weby/components/weby_news_slider/weby_news_slider_component.rb
@@ -1,3 +1,10 @@
 class WebyNewsSliderComponent < Component
   component_settings :news_category
+
+  def pages(site)
+    pages = news_category.blank? ? site.pages.limit(5) :
+      site.pages.tagged_with(news_category)
+
+    pages.find_all{|i| !i.image.nil? }
+  end
 end
