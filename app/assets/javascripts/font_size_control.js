dee21057febfc59beaaa0c67fcbba3db22f95abe
ricardops <ricardops.inf@gmail.com>
2012-06-22 14:10:24 -0300

    Corrigindo problem com zoom do texto no menu de acessibilidade

--- app/assets/javascripts/font_size_control.js
+++ app/assets/javascripts/font_size_control.js
@@ -1,40 +1,32 @@
 //= require jquery.cookie
 //= require_self
 
-$(document).ready(function(){
-    $.cookie("font_size_original", $('html').css('font-size'), { path: '/' })
-		
-		if($.cookie("font_size") != null)
-		$('html').css('font-size', $.cookie("font_size")+ 'px')
+var rejectTag = 'a';
+var tags = $('#wrapper,th,input').not(rejectTag)
+
+tags.ready(function(){
+  $.cookie("font_size_original", tags.css('font-size'), {path: '/' });
+  tags.css('font-size', null + 'px');
 })
 
 function font_size_up(){
-    var font = $('html').css('font-size'),
-    size = parseInt(font.substr(0, font.length - 2)) + 2
+  var font = tags.css('font-size');
+  var currentSizeTag = parseInt(font.substr(0, font.length - 2));
+  var newSizeTag = currentSizeTag + 2;
 
-    if( size < ( parseInt($.cookie("font_size_original").substr(0, font.length -2)) +10 ) )
-        $('html').css('font-size', size.toString() + 'px') 
-	
-		//atualiza valor de font_size no cookie
-		$.cookie("font_size", size, { path: '/' })
+  if( newSizeTag < ( parseInt($.cookie("font_size_original").substr(0, font.length - 2)) + 10 ) )
+    tags.css('font-size', newSizeTag.toString() + 'px');
 }
 
 function font_size_down(){
- 	var font = $('html').css('font-size'),
-    size = parseInt(font.substr(0, font.length - 2)) - 1
-    
-    if( size > ( parseInt($.cookie("font_size_original").substr(0, $.cookie("font_size_original").length -2))-3) )
-        $('html').css('font-size', size.toString() + 'px') 
+  var font = tags.css('font-size');
+  var currentSizeTag = parseInt(font.substr(0, font.length - 2));
+  var newSizeTag = currentSizeTag - 1;
 
-		//atualiza valor de font_size no cookie
-		$.cookie("font_size", size, { path: '/' })
-
+  tags.css('font-size', newSizeTag.toString() + 'px');
 }
 
-
 function font_size_original(){ 
- 		
-		$('html').css('font-size', $.cookie("font_size_original"))
-        $.cookie("font_size", null, {path: '/'})
- 
- }
+  tags.css('font-size', $.cookie("font_size_original"));
+  $.cookie("font_size", null, {path: '/'});
+}
