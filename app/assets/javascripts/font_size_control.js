a1ed93d6ed0bae096ea2775221056c660da7d65c
Jônatas Rancan <jonatasrancan@gmail.com>
2012-06-25 16:57:19 -0300

    arrumando bug na funcionalidade do tamanho das fontes

--- app/assets/javascripts/font_size_control.js
+++ app/assets/javascripts/font_size_control.js
@@ -1,16 +1,16 @@
 //= require jquery.cookie
 //= require_self
 var tags = $('*');
-var tags = tags.not(tags.find(".no_contrast").find("*"));
+var tags_font = tags.not(tags.find(".no_contrast").find("*"));
 var font_original = new Array();
 var font_up = new Array();
 var font_down = new Array();
 
 //setando cookies das fontes
-tags.ready(function(){
-  tags.each(function(){ font_original.push($(this).css('font-size'))})
+tags_font.ready(function(){
+  tags_font.each(function(){ font_original.push($(this).css('font-size'))})
   $.cookie("font_size_original", font_original, {path: '/' });
-  tags.css('font-size', null + 'px');
+  tags_font.css('font-size', null + 'px');
   for (var i = 0; i < font_original.length; i++) {
     font_up[i] = font_original[i];
     font_down[i] = font_original[i];
@@ -20,7 +20,7 @@ tags.ready(function(){
 
 function font_size_up(){
  var cont = 0;
-  tags.each(function  () {
+  tags_font.each(function  () {
     var currentSizeTag = parseInt(font_up[cont].substr(0, font_up[cont].length - 2));
     var newSizeTag = currentSizeTag + 2;
     
@@ -33,7 +33,7 @@ function font_size_up(){
 
 function font_size_down(){
   var cont = 0;
-  tags.each(function  () {
+  tags_font.each(function  () {
     var currentSizeTag = parseInt(font_down[cont].substr(0, font_down[cont].length - 2));
     var newSizeTag = currentSizeTag -2;
     
@@ -45,7 +45,7 @@ function font_size_down(){
 
 function font_size_original(){
 var i=0; 
-  tags.each(function(){
+  tags_font.each(function(){
     $(this).css('font-size',font_original[i++]);
   })
   for (var i = 0; i < font_original.length; i++) {
