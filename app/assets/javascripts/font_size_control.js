392cc9a131c6243e2d6bb86a6e07c6f70b478dbb
Jônatas Rancan <jonatasrancan@gmail.com>
2012-06-25 17:24:47 -0300

    arrumando conflito no merge

--- app/assets/javascripts/font_size_control.js
+++ app/assets/javascripts/font_size_control.js
@@ -8,48 +8,48 @@ var font_down = new Array();
 
 //setando cookies das fontes
 tags_font.ready(function(){
-  tags_font.each(function(){ font_original.push($(this).css('font-size'))})
-  $.cookie("font_size_original", font_original, {path: '/' });
-  tags_font.css('font-size', null + 'px');
-  for (var i = 0; i < font_original.length; i++) {
-    font_up[i] = font_original[i];
-    font_down[i] = font_original[i];
-  };
+   tags_font.each(function(){ font_original.push($(this).css('font-size'))})
+   $.cookie("font_size_original", font_original, {path: '/' });
+tags_font.css('font-size', null + 'px');
+for (var i = 0; i < font_original.length; i++) {
+   font_up[i] = font_original[i];
+   font_down[i] = font_original[i];
+};
 })
 
 
 function font_size_up(){
- var cont = 0;
-  tags_font.each(function  () {
-    var currentSizeTag = parseInt(font_up[cont].substr(0, font_up[cont].length - 2));
-    var newSizeTag = currentSizeTag + 2;
-    
-    font_up[cont] = newSizeTag.toString() + 'px';
-     
-    $(this).css('font-size', font_up[cont++] );
-  })
-  
+   var cont = 0;
+   tags_font.each(function  () {
+      var currentSizeTag = parseInt(font_up[cont].substr(0, font_up[cont].length - 2));
+      var newSizeTag = currentSizeTag + 2;
+
+      font_up[cont] = newSizeTag.toString() + 'px';
+
+      $(this).css('font-size', font_up[cont++] );
+   })
+
 }
 
 function font_size_down(){
-  var cont = 0;
-  tags_font.each(function  () {
-    var currentSizeTag = parseInt(font_down[cont].substr(0, font_down[cont].length - 2));
-    var newSizeTag = currentSizeTag -2;
-    
-    font_down[cont] = newSizeTag.toString() + 'px';
-    
-    $(this).css('font-size', font_down[cont++] );
-  })
+   var cont = 0;
+   tags_font.each(function  () {
+      var currentSizeTag = parseInt(font_down[cont].substr(0, font_down[cont].length - 2));
+      var newSizeTag = currentSizeTag -2;
+
+      font_down[cont] = newSizeTag.toString() + 'px';
+
+      $(this).css('font-size', font_down[cont++] );
+   })
 }
 
 function font_size_original(){
-var i=0; 
-  tags_font.each(function(){
-    $(this).css('font-size',font_original[i++]);
-  })
-  for (var i = 0; i < font_original.length; i++) {
-    font_up[i] = font_original[i];
-    font_down[i] = font_original[i];
-  };
+   var i=0; 
+   tags_font.each(function(){
+      $(this).css('font-size',font_original[i++]);
+   })
+   for (var i = 0; i < font_original.length; i++) {
+      font_up[i] = font_original[i];
+      font_down[i] = font_original[i];
+   };
 }
