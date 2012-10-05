792d919b0b6d68433563da9ee66ab8a0fd97683d
Leonardo R Souza <porkinhu@gmail.com>
2012-10-05 18:17:20 -0300

    Configuração de visibilidade do componente html

--- lib/weby/components/blank/blank_component.rb
+++ lib/weby/components/blank/blank_component.rb
@@ -1,5 +1,11 @@
 class BlankComponent < Component
-  component_settings :body
+  component_settings :body, :visibility
 
   validates :body, presence: true
+
+  alias :_visibility :visibility
+  def visibility
+    _visibility.blank? ? 0 : _visibility.to_i
+  end
+
 end
