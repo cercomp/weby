821a1d1c6292f300b5ebbbfe0d412c70e3e5f377
Jônatas Rancan <jonatasrancan@gmail.com>
2012-10-10 12:47:24 -0300

    resolve #618 , grupos do fale conosco podem ser configurados para aparecerem marcados por padrao

--- lib/weby/components/feedback/feedback_component.rb
+++ lib/weby/components/feedback/feedback_component.rb
@@ -1,5 +1,20 @@
 class FeedbackComponent < Component
-  component_settings :label
+  component_settings :label, :groups_id 
 
   validates :label, presence: true
+
+
+  def parse_groups(site)
+    groups_site = site.groups 
+    "".tap do |group_names|
+      groups_site.each do |group|
+        group_names << group.name + "," if groups_id.include? group.id.to_s 
+      end
+    end
+  end
+
+  def checked?(id)
+    groups_id.include? id.to_s
+  end
+
 end
