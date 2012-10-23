a94881530126fc99f6a946c44543a76c27c26bd9
Jônatas Rancan <jonatasrancan@gmail.com>
2012-10-23 11:47:38 -0200

    resolve #630, modo de destaque como opcao no componente, e tamanho da tag varia com a frequencia da tag

--- lib/weby/components/weby_tag_cloud/weby_tag_cloud_component.rb
+++ lib/weby/components/weby_tag_cloud/weby_tag_cloud_component.rb
@@ -1,8 +1,19 @@
 class WebyTagCloudComponent < Component
-  component_settings :cloud_type, :width, :height, :color, :hoover_color, :font_size
+  component_settings :cloud_type, :width, :height, :color, :hoover_color, :hoover_type
 
   def tags(site)
     site.pages.published.tag_counts_on(:categories).map { |tag| tag.name }
+  end
+
+  def tag_size(tag,site)
+    max_font_size = 35
+    min_font_size = 15
+    min_occurs = tag_count(site).min
+    max_occurs = tag_count(site).max
+    tag_occurs = site.pages.published.tagged_with(tag).count
+    
+    weight = (Math.log(tag_occurs)-Math.log(min_occurs))/(Math.log(max_occurs)-Math.log(min_occurs))
+    min_font_size + ((max_font_size-min_font_size)*weight).round
   end
 
   alias :_width :width
@@ -15,9 +26,11 @@ class WebyTagCloudComponent < Component
     _height.blank? ? '300' : _height
   end
 
-  alias :_font_size :font_size
-  def font_size
-    _font_size.blank? ? '15' : _font_size
+  def tag_count(site)
+    [].tap do |tag_count|
+      site.pages.published.category_counts.each do |tag|
+        tag_count << tag.count
+      end
+    end
   end
-
 end
