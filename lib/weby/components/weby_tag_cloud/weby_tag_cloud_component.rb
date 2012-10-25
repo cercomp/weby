174f3864be4844b7b3368b9975f8523801785387
Jônatas Rancan <jonatasrancan@gmail.com>
2012-10-25 13:12:00 -0200

    Resolvendo bug e criando a opcao de velocidade na nuvem de tags

--- lib/weby/components/weby_tag_cloud/weby_tag_cloud_component.rb
+++ lib/weby/components/weby_tag_cloud/weby_tag_cloud_component.rb
@@ -1,5 +1,7 @@
 class WebyTagCloudComponent < Component
-  component_settings :cloud_type, :width, :height, :color, :hoover_color, :hoover_type
+  component_settings :cloud_type, :width, :height, :color, :hoover_color, :hoover_type, :speed
+
+  validates :speed, :numericality => {:greater_than => 0}
 
   def tags(site)
     site.pages.published.tag_counts_on(:categories).map { |tag| tag.name }
@@ -8,10 +10,12 @@ class WebyTagCloudComponent < Component
   def tag_size(tag,site)
     max_font_size = 35
     min_font_size = 15
-    min_occurs = tag_count(site).min
-    max_occurs = tag_count(site).max
+    occurs = tag_count(site)
+    min_occurs = occurs.min
+    max_occurs = occurs.max
     tag_occurs = site.pages.published.tagged_with(tag).count
-    
+    tag_occurs = 1 if tag_occurs < 1
+
     weight = (Math.log(tag_occurs)-Math.log(min_occurs))/(Math.log(max_occurs)-Math.log(min_occurs))
     min_font_size + ((max_font_size-min_font_size)*weight).round
   end
@@ -24,6 +28,11 @@ class WebyTagCloudComponent < Component
   alias :_height :height
   def height
     _height.blank? ? '300' : _height
+  end
+
+  alias :_speed :speed
+  def speed
+    _speed.blank? ? '5' : _speed
   end
 
   def tag_count(site)
