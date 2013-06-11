5e58eeab33e2c46e7d906b7c3b76c18b29805ef8
Nícolas Lazarte Kaqui <nicolaslazartekaqui@gmail.com>
2013-06-11 00:12:09 -0300

    Adicionado TODO onde está quebrando o db:migrate

--- lib/weby/components.rb
+++ lib/weby/components.rb
@@ -20,6 +20,7 @@ module Weby
         config[:group] = :weby
       end
 
+      # TODO: Fix, está quebrando quando a tabela components não existe na base
       require "weby/components/#{comp_name}/#{comp_name}_component"
       @components[comp_name.to_sym] = config
     end
