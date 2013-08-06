0f5e380135d9063b693f1327502b8d83e9454626
Arthur Henrique Guimarães <warthur2@gmail.com>
2013-08-06 10:56:10 -0300

    Mudanças em clipping

--- app/helpers/clipping_helper.rb
+++ app/helpers/clipping_helper.rb
@@ -3,7 +3,7 @@ module ClippingHelper
 
   def news_image(file, format, width = nil, height = nil, options = {})
     if file.nil?
-     image_tag("weby-filler.png") 
+     link_to image_tag("weby-filler.png"), options[:url]
     else
       weby_file_view(file, format, width, height, options)
     end
