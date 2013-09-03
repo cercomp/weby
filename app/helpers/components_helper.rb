10dd5acfac59bfa9497a34d54bcf486c153ef6dd
Leonardo R Souza <porkinhu@gmail.com>
2013-09-03 16:06:21 -0300

    Group components sort

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -15,22 +15,33 @@ module ComponentsHelper
   end
 
   def list_component(compo, name)
-      exceptions = ["show"] 
-      exceptions << "edit" unless component_is_available(compo.name) 
-      components_html = ""
-      components_html <<  (" <li id='sort_sites_component_#{compo.id }' class='component component-#{ compo.name } #{'disabled' unless component_is_available(compo.name)}' data-place='#{name}'>
-        <div>
-          <span class='widget-name'>
-           #{ raw ("#{toggle_field(compo, "publish")} #{t("components.#{compo.name}.name")} - #{compo.alias || compo.default_alias}") }
-         </span>
-         <div class='pull-right'>
-           #{ raw make_menu(compo, :except => exceptions, :with_text => false) } 
-           #{ "<span class='handle'>#{icon('move') }</span>" if check_permission(Sites::Admin::ComponentsController, 'sort') }
-         </div>
-         <div class='clearfix'></div>
-        </div>
-      </li>")
-    return components_html
+    exceptions = ["show"]
+    exceptions << "edit" unless component_is_available(compo.name)
+    components_html = ""
+    components_html <<  (" <li id='sort_sites_component_#{compo.id }' class='component component-#{ compo.name } #{'disabled' unless component_is_available(compo.name)}' data-place='#{name}'>
+      <div>
+        <span class='widget-name'>
+         #{ raw ("#{toggle_field(compo, "publish")} #{t("components.#{compo.name}.name")} - #{compo.alias || compo.default_alias}") }
+       </span>
+       <div class='pull-right'>
+         #{ raw make_menu(compo, :except => exceptions, :with_text => false) }
+         #{ "<span class='handle'>#{icon('move') }</span>" if check_permission(Sites::Admin::ComponentsController, 'sort') }
+         #{ link_to t("+"), new_site_admin_component_path(placeholder: compo.alias), class: "btn btn-success btn-small" if compo.name.to_s == "components_group" and check_permission(Sites::Admin::ComponentsController, [:new]) }
+       </div>
+       <div class='clearfix'></div>
+      </div>")
+    if compo.name.to_s == "components_group"
+      components_per_group = @components.select{|comp| comp.place_holder == compo.alias}
+      @components = @components - components_per_group
+      components_html << "<div class=\"order-list\" data-place=\"#{compo.alias}\"><ul>"
+      components_per_group.each do |comp|
+        component = Weby::Components.factory(comp)
+        components_html << list_component(component, compo.alias)
+      end
+      components_html << "</ul></div>"
+    end
+    components_html << "</li>"
+    return components_html.html_safe
   end
 
   # Busca os componentes existentes no sistema de forma ordenada pelo i18n
