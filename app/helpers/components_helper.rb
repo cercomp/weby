2e8cf2099eb50988a971e23ee83ec3adef8f5cd3
Leonardo R Souza <porkinhu@gmail.com>
2013-09-06 15:17:14 -0300

    Title on add button

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -27,7 +27,7 @@ module ComponentsHelper
         <div class='pull-right'>
           #{ raw make_menu(compo, :except => exceptions, :with_text => leftout) }
           #{ "<span class='handle'>#{icon('move') }</span>" if check_permission(Sites::Admin::ComponentsController, 'sort') and !leftout }
-          #{ link_to t("+"), new_site_admin_component_path(placeholder: compo.id), class: "btn btn-success btn-small" if compo.name.to_s == "components_group" and check_permission(Sites::Admin::ComponentsController, [:new]) and !leftout }
+          #{ link_to t("+"), new_site_admin_component_path(placeholder: compo.id), class: "btn btn-success btn-small", title: t(".new_component") if compo.name.to_s == "components_group" and check_permission(Sites::Admin::ComponentsController, [:new]) and !leftout }
         </div>
         <div class='clearfix'></div>
       </div>"
