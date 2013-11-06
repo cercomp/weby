830e05bc308391a8481c13d7a8cfccdfe9a41f73
Arthur Henrique Guimarães <warthur2@gmail.com>
2013-11-06 10:50:42 -0200

    dropdown i18n menu

--- lib/weby/components/menu_i18n/menu_i18n_component.rb
+++ lib/weby/components/menu_i18n/menu_i18n_component.rb
@@ -1,3 +1,9 @@
 class MenuI18nComponent < Component
-  component_settings
+  component_settings :dropdown
+
+  alias :_dropdown :dropdown
+  def dropdown
+    _dropdown.blank? ? false : _dropdown.to_i == 1
+  end
+
 end
