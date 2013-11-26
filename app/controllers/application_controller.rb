5db736c5aa91dc1df6cbf34834807497a36a74ca
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2013-11-26 09:53:54 -0200

    Iniciando umas correções.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -305,16 +305,19 @@ class ApplicationController < ActionController::Base
 
   def record_activity(note, loggeable)
     @activity = ActivityRecord.new
-    @activity.user_id = current_user.id
-    @activity.site_id = current_site.id
-    @activity.note = note
-    @activity.browser = request.env['HTTP_USER_AGENT']
-    @activity.ip_address = request.env['REMOTE_ADDR']
-    @activity.controller = controller_name
-    @activity.action = action_name
-    @activity.params = params.inspect
-    @activity.loggeable = loggeable
-    @activity.save
+    if !ActivityRecord.where(user_id: current_user.id, site_id: current_site.id, controller_name, action: action_name, loggeable: loggeable, 
+#      @activity = ActivityRecord.new
+      @activity.user_id = current_user.id
+      @activity.site_id = current_site.id
+      @activity.note = note
+      @activity.browser = request.env['HTTP_USER_AGENT']
+      @activity.ip_address = request.env['REMOTE_ADDR']
+      @activity.controller = controller_name
+      @activity.action = action_name
+      @activity.params = params.inspect
+      @activity.loggeable = loggeable
+      @activity.save
+    end
   end
 
 end
