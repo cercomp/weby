17d35d1e864ef21049c7e6e9765a66e3a7c4f5e9
Leonardo R Souza <porkinhu@gmail.com>
2013-12-24 09:38:58 -0200

    Mudança no tempo de bloqueio - devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -171,7 +171,7 @@ Devise.setup do |config|
   config.maximum_attempts = 20
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  config.unlock_in = 1.hour
+  config.unlock_in = 10.minutes
 
   # Warn on the last attempt before the account is locked.
   config.last_attempt_warning = true
