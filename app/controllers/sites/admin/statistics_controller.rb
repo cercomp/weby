37e41d66c172049de40903925ae6160b7fcd6b9e
leo-souza <porkinhu@gmail.com>
2014-01-15 13:44:22 -0200

    Correção bug período de meses das estatísticas

--- app/controllers/sites/admin/statistics_controller.rb
+++ app/controllers/sites/admin/statistics_controller.rb
@@ -1,4 +1,5 @@
 class Sites::Admin::StatisticsController < ApplicationController
+  include DateHelper
   before_filter :require_user
   before_filter :check_authorization
   
@@ -7,14 +8,7 @@ class Sites::Admin::StatisticsController < ApplicationController
   def index
     respond_to do |format|
       format.html do
-        min_date = current_site.views.minimum(:created_at)
-        @years = min_date ? Time.now.year.downto(min_date.year).to_a : [Time.now.year]
-        @months = []
-        curr_date, oldest_date = Date.current, min_date ? min_date.to_date : Date.current
-        while curr_date.month >= oldest_date.month && curr_date.year >= oldest_date.year
-          @months << [l(curr_date, format: :monthly), curr_date.strftime('%m/%Y')]
-          curr_date -= 1.month
-        end
+        @years, @months = map_months_from_date current_site.views.minimum(:created_at)
       end
       format.json do
         case params[:type]
--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -14,4 +14,14 @@ module DateHelper
   def publish_date?(obj)
     obj.date_begin_at <= Time.now and obj.date_end_at > Time.now
   end
+
+  def map_months_from_date end_date
+    end_date ||= Time.now
+    months, curr_date = [], Time.now
+    while curr_date.beginning_of_month >= end_date.beginning_of_month
+      months << [l(curr_date.to_date, format: :monthly), curr_date.strftime('%m/%Y')] #TODO use a block for cutsom format
+      curr_date -= 1.month
+    end
+    [Time.now.year.downto(end_date.year).to_a, months]
+  end
 end
