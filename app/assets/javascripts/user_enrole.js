b5892915a91e8e8abd7aac6cc999d6eb16a57431
leo-souza <porkinhu@gmail.com>
2014-01-30 13:38:47 -0200

    Correção de bugs

---
+++ app/assets/javascripts/user_enrole.js
@@ -0,0 +1,37 @@
+/*input: texto pesquisado
+ *list: lista a ser filtrada <menu> ou <ul> */
+function filter_user(input, list){
+    var pattern = new RegExp(".*"+input+".*", "gi"),
+        $list = $(list);
+    $list.find('.no-elements-filter-user').remove();
+    $list.find('li').each(function(idx, element){
+      if( $(element).text().match(pattern) )
+        $(element).show();
+      else
+        $(element).hide();
+    });
+    if($list.find('li:visible').length == 0){
+      $list.append('<li class="no-elements-filter-user alert">Nenhum usu&aacute;rio encontrado. ('+input+')</li>')
+    }
+}
+
+var initEnrolePage = function(){
+  var list = $('#users_list_roles'),
+      field = $('#users_unroled');
+  field.keypress(function(ev){
+    if(ev.keyCode == 13){
+      filter_user($(this).val(), list);
+      ev.stopPropagation();
+      ev.preventDefault();
+      return false;
+    }
+  });
+
+  var fullname = field.data('fullname');
+  if(fullname.length > 0){
+    field.val(fullname);
+    filter_user(fullname, list);
+  }
+}
+
+$(document).ready(initEnrolePage);
--- app/helpers/locales_helper.rb
+++ app/helpers/locales_helper.rb
@@ -10,15 +10,16 @@ module LocalesHelper
   end
 
   def each_i18n_tab locales=current_site.locales
+    @tab_count = @tab_count.to_i + 1
     content_tag :div, class: "tabbable i18n" do
       tabs = content_tag :ul, class: "nav nav-tabs" do
         locales.each_with_index.map do |locale, index|
-          content_tag :li, link_to(locale_with_name(locale, '16'), "#tab_#{locale}", data: {toggle: 'tab'}), class: (index == 0 ? "active" : "")
+          content_tag :li, link_to(locale_with_name(locale, '16'), "#tab_#{locale}#{@tab_count}", data: {toggle: 'tab'}), class: (index == 0 ? "active" : "")
         end.join('').html_safe
       end
       content = content_tag :div, class: "tab-content" do
         locales.each_with_index.map do |locale, index|
-          content_tag :div, id: "tab_#{locale.name}", class: "tab-pane#{index == 0 ? " active" : ""}" do
+          content_tag :div, id: "tab_#{locale.name}#{@tab_count}", class: "tab-pane#{index == 0 ? " active" : ""}" do
             yield(locale) if block_given?
           end
         end.join('').html_safe
