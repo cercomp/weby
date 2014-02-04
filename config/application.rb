044e276438375096e8a436a90c2c275e39560c02
Nícolas Lazarte Kaqui <nicolaslazartekaqui@gmail.com>
2014-02-04 01:34:29 -0200

    Adicionando estrutura de concerns já no padrão do rails 4 * corrigido as permissões de alguns arquivos

--- config/application.rb
+++ config/application.rb
@@ -16,47 +16,29 @@ module Weby
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W( #{config.root}/lib #{config.root}/lib/weby/**/* )
+    config.autoload_paths += %W(
+      #{config.root}/lib
+      #{config.root}/app/models/concerns
+      #{config.root}/app/controllers/concerns
+    )
     config.autoload_paths += Dir["#{config.root}/lib/weby/components/**/*"]
     config.autoload_paths += Dir["#{config.root}/vendor/engines/*/lib/weby/components/**/*"]
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
     config.active_record.observers = :page_position_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-    I18n.enforce_available_locales = false
-    config.i18n.load_path += Dir[Rails.root.join('**', 'locales', '**', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = "pt-BR"
+    config.i18n.load_path += Dir["#{config.root}/**/locales/**/*.yml"]
+    config.i18n.default_locale = 'pt-BR'
 
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    I18n.enforce_available_locales = false
 
     # Configure sensitive parameters which will be filtered from the log file.
-    # config.filter_parameters += [:password]
     config.filter_parameters << :password << :password_confirmation
-
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
-
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -66,11 +48,7 @@ module Weby
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    #config.assets.paths << Rails.root.join("app", "assets", "flash")
-    #paths_bkp = config.assets.paths
-    #config.assets.precompile = [/(?=.*weby)(?:\/|\\|\A)\w+\.(css|js)$/]
     config.assets.precompile << Proc.new { |path| !path.match(/bootstrap/) }
-    #config.assets.paths = [/(?=.*weby)/]
 
     config.i18n.fallbacks = true
 
