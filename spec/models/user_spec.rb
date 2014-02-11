8190998da8e0b187f3d41fd90bb7b28b4fc506b7
Arthur Henrique Guimarães <warthur2@gmail.com>
2014-02-11 16:32:31 -0200

    User tests #53

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,10 +8,14 @@ describe User do
   it { expect(subject).to validate_presence_of(:last_name) } 
 
   context 'email' do
-    pending 'should accept valid email addresses format' do
+    it 'should accept valid email addresses format' do
+      subject = build(:user) 
+      expect(subject.email).to match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i)
     end
 
-    pending 'should reject invalid email addresses format' do
+    it 'should reject invalid email addresses format' do
+      subject = build(:user, email: 'askdjasldasdjl.com')
+      expect(subject.email).not_to match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i)
     end
 
     it 'should validate uniqueness of email addresses' do
@@ -20,21 +24,34 @@ describe User do
       expect(subject).to validate_uniqueness_of(:email)
     end
 
-    pending 'should reject email addresses identical up to case' do
+    it 'should reject email addresses identical up to case' do
+      user = create(:user)
+      subject = build(:user, email: user.email.upcase)
+      expect(subject).to validate_uniqueness_of(:email)
     end
 
-    pending 'should change email into downcase before a save' do
+    it 'should change email into downcase before saving' do
+      subject= build(:user, email:"EMAIL@email.com")
+      subject.save
+      expect(subject.email).to eq(subject.email.downcase)
     end
   end
   
   context 'login' do
-    pending 'should accept valid login format' do
+    it 'should accept valid login format' do
+      subject = build(:user)
+      expect(subject.login).to match(/^[a-z\d_\-\.@]+$/i)
     end
 
-    pending 'should reject invalid login format' do
+    it 'should reject invalid login format' do
+      subject = build(:user, login: 'log#in')
+      expect(subject.login).not_to match(/^[a-z\d_\-\.@]+$/i)                            
     end
 
-    pending 'should validate uniqueness of login' do
+    it 'should validate uniqueness of login' do
+      user = create(:user)
+      subject = build(:user, login: user.email)
+      expect(subject).to validate_uniqueness_of(:login)
     end
 
   end
@@ -43,10 +60,14 @@ describe User do
     pending 'should confirm password' do
     end
 
-    pending 'should accept valid password format ' do
+    it 'should accept valid password format ' do
+      subject = build(:user)
+      expect(subject.password).to match(/(?=.*\d+)(?=.*[A-Z]+)(?=.*[a-z]+)^.{4,}$/)
     end
 
-    pending 'should reject invalid password format' do
+    it 'should reject invalid password format' do
+      subject = build(:user, password: "a1234")
+      expect(subject.password).not_to match(/(?=.*\d+)(?=.*[A-Z]+)(?=.*[a-z]+)^.{4,}$/)
     end
 
   end
