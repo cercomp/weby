f3e599947bd4d9cfebcfe3d91137626242f07b70
Marcos Roberto <robeiro@gmail.com>
2014-02-13 14:03:39 -0200

    Tarefas #18, #19 export and import styles

--- app/models/style.rb
+++ app/models/style.rb
@@ -69,11 +69,21 @@ class Style < ActiveRecord::Base
     return attrs.each{|attr| self.import attr, options } if attrs.is_a? Array
 
     attrs = attrs.dup
-    attrs = attrs['own_styles'] if attrs.has_key? 'own_styles'
-    attrs = attrs['follow_styles'] if attrs.has_key? 'follow_styles'
+    attrs = attrs['styles'] if attrs.has_key? 'styles'
 
-    attrs.except!('id', 'created_at', 'updated_at')
-    attrs['owner_id'] = options[:owner]
+    if attrs['style_id']
+      follow = Style.unscoped.find(attrs['style_id'])
+      if attrs['name'] == follow.name
+         attrs['css'] = nil
+         attrs['name'] = nil
+      else
+         attrs['style_id'] = nil
+      end
+
+    end
+
+    attrs.except!('id', 'created_at', 'updated_at', 'site_id')
+
     style = self.create!(attrs)
 
   end
