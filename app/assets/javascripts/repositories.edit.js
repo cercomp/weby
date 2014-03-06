756b7a70bbf4ea1811db46c6764c7e06a446f666
leo-souza <porkinhu@gmail.com>
2014-03-06 16:53:17 -0300

    Ajustes no js do crop image

--- app/assets/javascripts/repositories.edit.js
+++ app/assets/javascripts/repositories.edit.js
@@ -1,47 +1,53 @@
 //= require jquery.Jcrop.min
 
 $(document).ready(function () {
-  var jcrop_api;
+  var jcrop_api,
+      jcropInit = false;
+      userValue = false;
 
-  //Após o carregamento da imagem, iniciar o jCrop
-  $('#img-crop').load(function(){
-    $('.real-width').text(this.naturalWidth);
-    $('.real-height').text(this.naturalHeight);
-    $(this).Jcrop({
-      boxWidth: this.clientWidth,
-      boxHeight: this.clientHeight,
-      trueSize: [this.naturalWidth, this.naturalHeight],
-      onSelect: function (selection) {
-        $("#repository_x").val(Math.ceil(selection.x));
-        $("#repository_y").val(Math.ceil(selection.y));
-        $("#repository_w").val(Math.ceil(selection.w));
-        $("#repository_h").val(Math.ceil(selection.h));
-      },
-      onRelease: function(){
-        $("#repository_x").val(null);
-        $("#repository_y").val(null);
-        $("#repository_w").val(null);
-        $("#repository_h").val(null);
+  var setValue = function(input, data, value){
+    if ($("#repository_"+input).data('raw') != data){
+      $("#repository_"+input).data('raw', data);
+      if (typeof(value) == 'undefined' || value == null){
+        value = Math.round(data);
+      }else if (value % 1 != 0){
+        value = Math.round(value);
       }
-    }, function(){
-      jcrop_api = this;
-    });
-    $('.img-edit .panel-body').hide();
-  });
-  //Iniciar o carregamento da imagem manualmente
-  $('#img-crop').attr('src', $('#img-crop').data('src'));
-
-  $('.toggle-lock').click(function(){
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
-      jcrop_api.setOptions({aspectRatio: 0})
-    }else{
-      $(this).addClass('active');
-      var o = jcrop_api.tellSelect();
-      jcrop_api.setOptions({aspectRatio: parseInt(o.w)/parseInt(o.h)})
+      $("#repository_"+input).val(value);
     }
-    return false;
-  });
+  }
+
+  var initJcrop = function(){
+    //Após o carregamento da imagem, iniciar o jCrop
+    $('#img-crop').load(function(){
+      $('.real-size').html('<b>('+this.naturalWidth+'x'+this.naturalHeight+')</b>');
+      $(this).Jcrop({
+        boxWidth: this.clientWidth,
+        boxHeight: this.clientHeight,
+        trueSize: [this.naturalWidth, this.naturalHeight],
+        onSelect: function (selection) {
+          if (!userValue){
+            setValue('x', selection.x);
+            setValue('y', selection.y);
+            setValue('w', selection.w);
+            setValue('h', selection.h);
+          }
+          userValue = false;
+        },
+        onRelease: function(){
+          $("#repository_x").val(null);
+          $("#repository_y").val(null);
+          $("#repository_w").val(null);
+          $("#repository_h").val(null);
+        }
+      }, function(){
+        jcrop_api = this;
+      });
+    });
+    //Iniciar o carregamento da imagem manualmente
+    $('#img-crop').attr('src', $('#img-crop').data('src'));
+    jcropInit = true;
+  }
 
   //Quando o usuário seta os valores, muda a seleção na imagem
   $('#repository_w,#repository_h').change(function(){
@@ -61,8 +67,37 @@ $(document).ready(function () {
         w = h * aspect;
       }
     }
-
+    userValue = true;
     jcrop_api.setSelect([x, y, x+w, y+h]);
+    var o = jcrop_api.tellSelect();
+    //Se o usuário seta um valor que corta pra fora da imagem,
+    //muda o valor do campo para o valor real
+    if(Math.abs(w-o.w) > 2){
+      w = null;
+      if(aspect != 0){
+        h = null;
+      }
+    }
+    if(Math.abs(h-o.h) > 2){
+      h = null;
+      if(aspect != 0){
+        w= null;
+      }
+    }
+    setValue('w', o.w, w);
+    setValue('h', o.h, h);
+  });
+
+  $('.toggle-lock').click(function(){
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      jcrop_api.setOptions({aspectRatio: 0})
+    }else{
+      $(this).addClass('active');
+      var o = jcrop_api.tellSelect();
+      jcrop_api.setOptions({aspectRatio: Math.round(o.w)/Math.round(o.h)})
+    }
+    return false;
   });
 
   //Simular o efeito de onchange quando pressiona [enter] e impedir que o formulário seja enviado
@@ -85,6 +120,8 @@ $(document).ready(function () {
       var reader = new FileReader();
       reader.onload = function(evt){
         img[0].src = evt.target.result;
+        img.parent('a').attr('href', '#');
+        img.parent('a').attr('target', null);
         img.show();
       };
       reader.readAsDataURL(this.files[0]);
@@ -93,7 +130,11 @@ $(document).ready(function () {
 
   $('.panel-heading').click(function(){
     var $body = $('.img-edit .panel-body');
-    $body.slideDown(300);
+    $body.slideDown(300, function(){
+      if(!jcropInit){
+        initJcrop();
+      }
+    });
     $body.prev('.panel-heading').removeClass('closed').addClass('open');
   });
 
@@ -101,6 +142,8 @@ $(document).ready(function () {
     var $body = $('.img-edit .panel-body');
     $body.slideUp(300);
     $body.prev('.panel-heading').removeClass('open').addClass('closed');
+    $('.toggle-lock').removeClass('active');
+    jcrop_api.setOptions({aspectRatio: 0})
     jcrop_api.release();
     return false;
   });
