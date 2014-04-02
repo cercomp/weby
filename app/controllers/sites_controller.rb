188d11eaf05de4ed4df24da7355b3ccd767da91e
Danillo Parreira <parreira16@gmail.com>
2014-04-02 08:00:23 -0300

    Alteração do espaçamento

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -15,7 +15,7 @@ class SitesController < ApplicationController
 
     #TODO Pesquisar também no título das notícias
     @sites = Site.ordered_by_front_pages(params[:search])
-		@sites = list_sites(@sites)
+    @sites = list_sites(@sites)
     @default_groupings = Weby::Settings.default_groupings
     if params[:groupings]
       @sites = @sites.includes(:groupings).where(groupings: {id: params[:groupings].split(',')}) unless params[:groupings] == 'all'
@@ -104,29 +104,29 @@ class SitesController < ApplicationController
   end
       
   def sites_hidden
-  	@group = Grouping.all
-  	ids_hidden = Array.new
-  	
-		@group.each do |g|
-			if g.hidden
-				g.site_ids.each do |id|
-					ids_hidden << id unless ids_hidden.include?(id)
-				end
-			end
-		end
-		ids_hidden
-	end
+  @group = Grouping.all
+  ids_hidden = Array.new
+
+  @group.each do |g|
+    if g.hidden
+      g.site_ids.each do |id|
+        ids_hidden << id unless ids_hidden.include?(id)
+      end
+    end
+  end
+  ids_hidden
+  end
 		
   def list_sites(site)
-  	unless site.nil?
-			site = Site.all
-  	end
-		if current_user.blank? || !current_user.is_admin
-			sites_hidden.each do |hidden|
-				site.delete(Site.find(hidden))
-			end
-		end
-		site
-	end
+    unless site.nil?
+      site = Site.all
+    end
+    if current_user.blank? || !current_user.is_admin
+      sites_hidden.each do |hidden|
+        site.delete(Site.find(hidden))
+      end
+    end
+    site
+  end
   
 end
