1c13e9c3ae275097ed0632a7fcd4b2e0d91c98a0
Matheus Castro <matheus7181@gmail.com>
2014-04-04 16:46:15 -0300

    Opção de escolha no menu de acessibilidade para tamanho de fonte e contraste.

--- lib/weby/components/menu_accessibility/menu_accessibility_component.rb
+++ lib/weby/components/menu_accessibility/menu_accessibility_component.rb
@@ -1,3 +1,11 @@
 class MenuAccessibilityComponent < Component
-  component_settings
+  component_settings :font_size, :contrast
+    
+  def font_size?
+    font_size.blank? ? false : font_size == "1"
+  end
+
+  def contrast?
+    contrast.blank? ? false :  contrast == "1"
+  end
 end
