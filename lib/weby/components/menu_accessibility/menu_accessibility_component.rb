ff69bb9de43934d97ddd47ab8ba005dbf88a382a
Matheus Castro <matheus7181@gmail.com>
2014-04-16 14:31:23 -0300

    Falta terminar o contraste personalizado.

--- lib/weby/components/menu_accessibility/menu_accessibility_component.rb
+++ lib/weby/components/menu_accessibility/menu_accessibility_component.rb
@@ -1,5 +1,5 @@
 class MenuAccessibilityComponent < Component
-  component_settings :font_size, :contrast, :own_contrast, :css_contrast
+  component_settings :font_size, :contrast, :own_contrast, :css_own_contrast
     
   def font_size?
     font_size.blank? ? false : font_size == "1"
@@ -7,5 +7,9 @@ class MenuAccessibilityComponent < Component
 
   def contrast?
     contrast.blank? ? false :  contrast == "1"
+  end
+
+  def own_contrast?
+    own_contrast.blank? ? false :  own_contrast == "1"
   end
 end
