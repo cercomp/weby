c68f80790a6c5118f43aa4e6158dfdef2471b531
Arthur Henrique Guimarães <warthur2@gmail.com>
2014-04-29 14:11:34 -0300

    Migrar categorias

--- db/migrate/20140402120348_create_sticker_banners.rb
+++ db/migrate/20140402120348_create_sticker_banners.rb
@@ -1,5 +1,6 @@
 class CreateStickerBanners < ActiveRecord::Migration
   def change
     rename_table :banners, :sticker_banners     
+    execute "update taggings set taggable_type='Sticker::Banner'  where taggable_type='Banner';"  
   end
 end
