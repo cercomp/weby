ea44ee5c559107633dbebc5c6066cef1a1a2cf01
Arthur Henrique Guimarães <warthur2@gmail.com>
2014-04-30 10:28:16 -0300

    Extensão banners: registro de atividades

--- db/migrate/20140402120348_create_sticker_banners.rb
+++ db/migrate/20140402120348_create_sticker_banners.rb
@@ -2,5 +2,6 @@ class CreateStickerBanners < ActiveRecord::Migration
   def change
     rename_table :banners, :sticker_banners     
     execute "update taggings set taggable_type='Sticker::Banner'  where taggable_type='Banner';"  
+    execute "update activity_records set loggeable_type='Sticker::Banner'  where loggeable_type='Banner';"  
   end
 end
