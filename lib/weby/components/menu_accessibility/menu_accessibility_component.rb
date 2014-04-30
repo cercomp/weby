93ecd452d6bd88d652c17539bbb89990226ba80c
Matheus Castro <matheus7181@gmail.com>
2014-04-30 14:16:05 -0300

    Rótulo de contraste parametrizável.

--- lib/weby/components/menu_accessibility/menu_accessibility_component.rb
+++ lib/weby/components/menu_accessibility/menu_accessibility_component.rb
@@ -4,11 +4,11 @@ class MenuAccessibilityComponent < Component
   i18n_settings :label_contrast
     
   def font_size?
-    font_size.blank? ? false : font_size == "1"
+    font_size.blank? ? true : font_size == "1"
   end
 
   def contrast?
-    contrast.blank? ? false :  contrast == "1"
+    contrast.blank? ? true :  contrast == "1"
   end
 
   def own_contrast?
