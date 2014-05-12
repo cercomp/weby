7faa6f7c4f12258f4c16655715c536484e0a98dc
Matheus Castro <matheus7181@gmail.com>
2014-05-12 16:50:55 -0300

    Alteração nas mensagens e rótulo para o tamanho de fonte.

--- lib/weby/components/menu_accessibility/menu_accessibility_component.rb
+++ lib/weby/components/menu_accessibility/menu_accessibility_component.rb
@@ -1,7 +1,7 @@
 class MenuAccessibilityComponent < Component
-  component_settings :font_size, :contrast, :label_contrast
+  component_settings :font_size, :contrast, :label_contrast, :label_font_size
 
-  i18n_settings :label_contrast
+  i18n_settings :label_contrast, :label_font_size
     
   def font_size?
     font_size.blank? ? true : font_size == "1"
