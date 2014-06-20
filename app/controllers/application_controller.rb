04335c17e40f86125b7613416374b3b180d62a10
Nícolas Lazarte Kaqui <nicolaslazartekaqui@gmail.com>
2014-06-20 10:38:27 -0300

    Erro em metodo que não existe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -99,7 +99,6 @@ class ApplicationController < ActionController::Base
     rescue_from Exception, with: :render_500
     rescue_from ActionController::RoutingError, with: :render_404
     rescue_from ActionController::UnknownController, with: :render_404
-    rescue_from ActionController::UnknownAction, with: :render_404
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
   end
 
