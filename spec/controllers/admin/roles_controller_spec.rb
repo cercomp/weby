faea7b0d3f107dbd2475010b52d2c6c574130fe0
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2014-06-20 11:15:40 -0300

    Teste do roles controller

--- spec/controllers/admin/roles_controller_spec.rb
+++ spec/controllers/admin/roles_controller_spec.rb
@@ -1,4 +1,70 @@
 require "spec_helper"
 
 describe Admin::RolesController do
+  let(:user) { FactoryGirl.create(:user, is_admin: true) }
+  let(:user_role) { FactoryGirl.create(:role) }
+
+  before { sign_in user }
+
+  describe "GET #index" do
+    before { get :index }
+
+    it "assigns @roles" do
+      expect(assigns(:roles)).to eq([user_role])
+    end
+
+    it "renders the index view" do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #edit" do
+    before { get :edit, :id => user_role.id }
+
+    it "assigns @role" do
+      expect(assigns(:role)).to eq(user_role)
+    end
+
+    it "renders the :edit view" do
+      expect(assigns(response)).to render_template(:edit)
+    end
+  end
+
+  describe "GET #new" do
+    before { get :new }
+
+    it "assigns @role" do
+      expect(assigns(:role)).to be_a_new(Role)
+    end
+
+    it "renders the :new view" do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST #create" do
+    context "when valid" do
+      before { post :create, :post => { :name => "Name" } }
+
+      it "will redirect to admin_roles path" do
+        expect(response).to redirect_to admin_roles_path
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "when success" do
+      it "will redirect to admin_roles path" do
+        expect(response).to be_success
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before { delete :update, :id => user_role.id }
+
+    it "will redirect to admin_roles_path" do
+      expect(response).to redirect_to admin_roles_path
+    end
+  end
 end
