8307a06ab929fc38ce6e8ae962c91a8b5c65e7b0
Arthur Henrique Guimarães <warthur2@gmail.com>
2014-06-26 09:31:29 -0300

    Validates format of component's classes

--- lib/weby/components/components_group/components_group_component.rb
+++ lib/weby/components/components_group/components_group_component.rb
@@ -1,5 +1,5 @@
 class ComponentsGroupComponent < Component
   component_settings :html_class
 
-  validates_format_of :html_class, with: /\A[A-Za-z0-9_\-]\z/
+  validates :html_class, format: { with: /\A[A-Za-z0-9_\-]*\z/ }
 end
--- lib/weby/components/image/image_component.rb
+++ lib/weby/components/image/image_component.rb
@@ -8,7 +8,7 @@ class ImageComponent < Component
     _new_tab.blank? ? false : _new_tab.to_i == 1
   end
 
-  validates_format_of :html_class, with: /\A[A-Za-z0-9_\-]*\z/
+  validates :html_class, format: { with: /\A[A-Za-z0-9_\-]*\z/ }
   validates :repository_id, presence: true
 
   belongs_to :target, polymorphic: true
