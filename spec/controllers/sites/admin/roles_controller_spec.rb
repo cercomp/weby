46050a0b65180a7de87d6401f74cbd4551804e1b
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2014-08-26 16:41:53 -0300

    roles controller tests

--- spec/controllers/sites/admin/roles_controller_spec.rb
+++ spec/controllers/sites/admin/roles_controller_spec.rb
@@ -9,18 +9,57 @@ describe Sites::Admin::RolesController do
  before { sign_in user }
 
   describe "GET #index" do
+    before { get :index }
+
+    pending "assigns @roles" do
+      expect(assigns(:roles)).to eq([roles])
+    end
+
+    pending "renders the :index view" do
+      expect(response).to render_template(:view)
+    end
   end
 
   describe "GET #show" do
+    before { get :show, :id => role.id }
+
+    pending "assigns @role" do
+      expect(assigns(:roles)).to eq([role])
+    end
+
+    pending "renders the :show view" do
+      expect(response).to redirect_to(site_admin_role_path(:roles))
+    end
   end
 
   describe "GET #new" do
+    before { get :new }
+
+    it "assigns @role" do
+      expect(assigns(:role)).to be_a_new(Role)
+    end
+
+    it "renders the :new view" do
+      expect(response).to render_template(:new)
+    end
   end
 
   describe "GET #edit" do
+    before { get :edit, :id => role.id }
+
+    it "assigns @role" do
+      expect(assigns(:role)).to eq(role)
+    end
   end
 
   describe "POST #create" do
+    before { post :create, :post => { :name => "Role" } }
+
+    it "" do
+    end
+
+    it "" do
+    end
   end
 
   describe "PUT #update" do
