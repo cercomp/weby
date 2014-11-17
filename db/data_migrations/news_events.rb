cd110beeb7393d2810f63980d54f4532fce8b1ef
leo-souza <porkinhu@gmail.com>
2014-11-17 16:15:52 -0200

    fix script

--- db/data_migrations/news_events.rb
+++ db/data_migrations/news_events.rb
@@ -1,34 +1,34 @@
-print('Updating Roles...')
-Role.find_each do |role|
-  if role.permissions && role.permissions.match(/^{/)
-    perm = eval(role.permissions)
-
-    actions = perm['pages'].dup
-
-    perm['news'] = actions.dup
-    actions.delete('sort')
-    perm['events'] = actions
-    perm['pages'] = actions
-    role.update_attribute :permissions, perm.to_s
-  end
-end
-print("OK\n")
-print("Updating Components...")
-Component.where(name: 'front_news').find_each do |component|
-  sett = eval(component.settings)
-  if sett['type_filter'] == 'events'
-    component.name = 'event_list'
-    sett['template'] = 'front'
-    sett.delete('order_by')
-  end
-  if sett['order_by'] == 'event_begin'
-    sett['order_by'] = 'created_at'
-  end
-  sett.delete('type_filter')
-  component.settings = sett.to_s
-  component.save
-end
-print("OK\n")
+#print('Updating Roles...')
+#Role.find_each do |role|
+#  if role.permissions && role.permissions.match(/^{/)
+#    perm = eval(role.permissions)
+#
+#    actions = perm['pages'].dup
+#
+#    perm['news'] = actions.dup
+#    actions.delete('sort')
+#    perm['events'] = actions
+#    perm['pages'] = actions
+#    role.update_attribute :permissions, perm.to_s
+#  end
+#end
+#print("OK\n")
+#print("Updating Components...")
+#Component.where(name: 'front_news').find_each do |component|
+#  sett = eval(component.settings)
+#  if sett['type_filter'] == 'events'
+#    component.name = 'event_list'
+#    sett['template'] = 'front'
+#    sett.delete('order_by')
+#  end
+#  if sett['order_by'] == 'event_begin'
+#    sett['order_by'] = 'created_at'
+#  end
+#  sett.delete('type_filter')
+#  component.settings = sett.to_s
+#  component.save
+#end
+#print("OK\n")
 print("Updating menu items...")
 MenuItem.find_each do |item|
   news = item.target
@@ -54,10 +54,10 @@ MenuItem.find_each do |item|
        }
      end
   )
-  news.update_attribute(:url, site_page_path(page))
+  news.update_attribute(:url, Rails.application.routes.url_helpers.site_page_path(page))
 
   item.target = page
-  item.url = site_page_path(page)
+  item.url = Rails.application.routes.url_helpers.site_page_path(page)
   item.save
 end
 print("OK\n")
@@ -74,7 +74,7 @@ Sticker::Banner.find_each do |banner|
     page = Page.find_by(id: match[1])
     if page
       banner.target = page
-      banner.url = site_page_path(page)
+      banner.url = Rails.application.routes.url_helpers.site_page_path(page)
       banner.save
     end
   end
