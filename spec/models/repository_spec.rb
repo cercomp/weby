88125915f5ede9869f9511e5f5cd0c00f78cc6e0
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2014-12-05 13:01:13 -0200

    fixing posts_repository model tests

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
 describe Repository do
-  it { expect(subject).to have_many(:page).with_foreign_key('repository_id') }
+  skip { expect(subject).to have_many(:posts).through(:posts_repositories) }
 
   it { expect(subject).to belong_to(:site) }
 
   it { expect(subject).to have_many(:posts_repositories).dependent(:destroy) }
-  it { expect(subject).to have_many(:pages).through(:posts_repositories) }
-
-  it { expect(subject).to have_many(:page_image).class_name('Page').dependent(:nullify) }
 end
