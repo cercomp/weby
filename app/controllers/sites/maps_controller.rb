e456eb2d0b9960fde056de36b96b76117195e0f6
Alexandre <alexandre.ufg@gmail.com>
2014-12-11 10:00:16 -0200

    Site map - delete show in controller

--- app/controllers/sites/maps_controller.rb
+++ app/controllers/sites/maps_controller.rb
@@ -1,16 +1,3 @@
 class Sites::MapsController < ApplicationController
   layout :choose_layout
-
-  def show
-#    @sitemap_global = {}
-#    current_site.menus.each do |globalmenu|
-#      if !globalmenu.root_menu_items.first.nil?
-#	@sitemap_global.merge!(globalmenu.id => globalmenu.name)
-#      end
-#      @sitemap_itens = globalmenu.items_by_parent(true)
-#      globalmenu.root_menu_items.each do |menu|
-#	menu.i18ns.first.title
-#      end
-#    end
-  end
 end
