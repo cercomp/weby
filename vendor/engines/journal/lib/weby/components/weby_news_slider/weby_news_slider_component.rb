5d584fadd446993b057a9d58d8cc9222766d5775
Marcos Roberto <robeiro@gmail.com>
2015-04-22 16:29:51 -0300

    Tarefa #24 fix bugs news_slide!!

--- vendor/engines/journal/lib/weby/components/weby_news_slider/weby_news_slider_component.rb
+++ vendor/engines/journal/lib/weby/components/weby_news_slider/weby_news_slider_component.rb
@@ -4,7 +4,7 @@ class WebyNewsSliderComponent < Component
   def news(site)
     news_category.blank? ?
       site.news_sites.where(site_id: site.id).joins(:news).available_fronts.where('journal_news.repository_id is not null').order('position desc').limit(quantity) :
-        site.news_sites.where(site_id: site.id).available.tagged_with(news_category.to_s.mb_chars.downcase.to_s).where('repository_id is not null').order('position desc')
+        site.news_sites.where(site_id: site.id).joins(:news).available.tagged_with(news_category.to_s.mb_chars.downcase.to_s).where('repository_id is not null').order('position desc')
   end
 
   alias_method :_width, :width
