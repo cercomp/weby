d0bb329c1bd3536e63df465df116b4d9fdf0b094
Marcos Roberto <robeiro@gmail.com>
2015-04-22 16:11:26 -0300

    Tarefa #24 fix bugs news_slide!!

--- vendor/engines/journal/lib/weby/components/weby_news_slider/weby_news_slider_component.rb
+++ vendor/engines/journal/lib/weby/components/weby_news_slider/weby_news_slider_component.rb
@@ -3,7 +3,7 @@ class WebyNewsSliderComponent < Component
 
   def news(site)
     news_category.blank? ?
-      site.news_sites.where(site_id: site.id).available_fronts.where('repository_id is not null').order('position desc').limit(quantity) :
+      site.news_sites.where(site_id: site.id).joins(:news).available_fronts.where('journal_news.repository_id is not null').order('position desc').limit(quantity) :
         site.news_sites.where(site_id: site.id).available.tagged_with(news_category.to_s.mb_chars.downcase.to_s).where('repository_id is not null').order('position desc')
   end
 
