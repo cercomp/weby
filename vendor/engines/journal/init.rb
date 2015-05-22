f4e2a3f732a8e86726f73067c04d738d886310e7
Marcos Roberto <robeiro@gmail.com>
2015-05-22 10:47:25 -0300

    Tarefa #226 extension news, setting author and news date !!

--- vendor/engines/journal/init.rb
+++ vendor/engines/journal/init.rb
@@ -1,3 +1,3 @@
 require 'weby'
 
-::Weby::register_extension(Weby::Extension.new(:journal, author: 'Cercomp - Equipe Web', menu_position: :before_pages))
+::Weby::register_extension(Weby::Extension.new(:journal, author: 'Cercomp - Equipe Web', menu_position: :before_pages, settings: [:author, :created_at, :updated_at]))
