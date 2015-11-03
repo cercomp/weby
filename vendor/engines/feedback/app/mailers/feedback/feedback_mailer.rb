38c663de1ad3946ffc48ff39bc54a79ce2d7e472
Marcello Moura <faraohh@gmail.com>
2015-11-03 09:12:30 -0200

    Use stmp_settings on from field
    Use 'ActionMailer::Base.smtp_settings[:user_name]' on 'from' field or 'feedback.mail'.

--- vendor/engines/feedback/app/mailers/feedback/feedback_mailer.rb
+++ vendor/engines/feedback/app/mailers/feedback/feedback_mailer.rb
@@ -3,7 +3,8 @@ module Feedback
     def send_feedback(feedback, destination, site)
       @feedback = feedback
       @site_title = site.title
-      mail(from: feedback.email, reply_to: feedback.email, to: destination, cc: feedback.email, subject: feedback.subject)
+      mail(from: ActionMailer::Base.smtp_settings[:user_name] || feedback.email,
+           reply_to: feedback.email, to: destination, cc: feedback.email, subject: feedback.subject)
     end
   end
 end
