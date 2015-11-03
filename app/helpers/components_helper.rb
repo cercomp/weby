a8c52175c82cf37c310230c38777f4f15d88e84e
Marcos Roberto <robeiro@gmail.com>
2015-11-03 11:46:08 -0200

    Tarefa #189 ajusts components group color !!

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -20,7 +20,12 @@ module ComponentsHelper
     components_html = '<li '
     components_html << "id='sort_sites_component_#{compo.id }' " unless leftout
     nickname = compo.alias.present? ? compo.alias : compo.default_alias
-    components_html << "class='component component-#{ compo.name } #{'disabled' unless component_is_available(compo.name)} #{compo.publish ? '' : 'deactivated'}' data-place='#{compo.place_holder}'>
+    if compo.name.to_s == 'components_group' 
+      components_html << "class='component-#{ compo.name } " 
+    else 
+      components_html << "class='component " 
+    end
+    components_html << "#{'disabled' unless component_is_available(compo.name)} #{compo.publish ? '' : 'deactivated'}' data-place='#{compo.place_holder}'>
       <div>
         <span class='widget-name'>
           #{ raw ("#{toggle_field(compo, 'publish')} #{t("components.#{compo.name}.name")} #{"- #{nickname}" if nickname.present?}") }
