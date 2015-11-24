30d2c5ba8de0c4225c1b79938525df3e35908ff7
leo-souza <porkinhu@gmail.com>
2015-11-24 14:13:46 -0200

    New email setting: domain

--- lib/weby/assets.rb
+++ lib/weby/assets.rb
@@ -1,8 +1,9 @@
 module Weby
   class Assets
     def self.asset_host_for(source, request)
-      settings = Weby::Settings::AssetHost
       return nil if !request
+
+      settings = Weby::Settings::AssetHost
       return nil if !(settings.assets_host.present? || settings.uploads_host.present?)
       return nil if request.ssl? && settings.disable_on_https
       return settings.assets_host if source.match(/^\/assets\//) && settings.assets_host.present?
