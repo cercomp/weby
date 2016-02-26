a7abc09ac4d1bbd74e92c39fa00027ee2afd8c6f
leo-souza <porkinhu@gmail.com>
2016-02-26 10:26:24 -0300

    [BugFix] Fix subsite news component bug

--- lib/weby/multisites.rb
+++ lib/weby/multisites.rb
@@ -6,13 +6,13 @@ module Weby
 
     module MultisiteInterface
       def acts_as_multisite
-        class_eval do
+        class_eval do |klass|
           scope :from_subsites, proc{|site, own = false|
-            where("site_id IN (#{((own ? [site] : [{ id: 0 }]) + site.subsites).select { |s| !s.blank? }.map { |s|s[:id] }.join(',')})").includes(:site)
+            where("#{klass.table_name}.site_id IN (#{((own ? [site] : [{ id: 0 }]) + site.subsites).select { |s| !s.blank? }.map { |s|s[:id] }.join(',')})").includes(:site)
           }
 
           scope :from_main_site, proc{|site, own = false|
-            where("site_id IN (#{((own ? [site] : [{ id: 0 }]) << site.main_site).select { |s| !s.blank? }.map { |s|s[:id] }.join(',')})").includes(:site)
+            where("#{klass.table_name}.site_id IN (#{((own ? [site] : [{ id: 0 }]) << site.main_site).select { |s| !s.blank? }.map { |s|s[:id] }.join(',')})").includes(:site)
           }
         end
       end
