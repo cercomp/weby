10b1cc93f5ca4d600bb7971c4a229505c86bfec8
leo-souza <porkinhu@gmail.com>
2018-10-22 15:10:37 -0300

    Added some more sections

--- vendor/engines/journal/lib/weby/components/subsite_front_news/subsite_front_news_component.rb
+++ vendor/engines/journal/lib/weby/components/subsite_front_news/subsite_front_news_component.rb
@@ -1,5 +1,5 @@
 class SubsiteFrontNewsComponent < Component
-  component_settings :quant, :source, :sel_site, :tag_as_label, :hide_author, :hide_read_more,
+  component_settings :quant, :source, :sel_site, :tag_as_label, :hide_author,
                      :label, :link_to_all
 
   i18n_settings :label, :link_to_all
@@ -10,10 +10,6 @@ class SubsiteFrontNewsComponent < Component
 
   def hide_author?
     hide_author.blank? ? false : hide_author.to_i == 1
-  end
-
-  def hide_read_more?
-    hide_read_more.blank? ? false : hide_read_more.to_i == 1
   end
 
   def tag_as_label?
