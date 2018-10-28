2173ab4fbb18348c94b361c563f822639bb78b2c
Danilo Resende <danilo.isr@gmail.com>
2018-10-28 16:26:15 -0300

    Improve admin/activity_records_controller specs

--- spec/controllers/admin/activity_records_controller_spec.rb
+++ spec/controllers/admin/activity_records_controller_spec.rb
@@ -6,45 +6,30 @@ describe Admin::ActivityRecordsController do
   before { sign_in user }
 
   describe "GET #index" do
-    skip "should populate an array of activities from the site being viewed" do
-      record = FactoryGirl.create(:activity_record)
+    it "renders all records" do
+      record = FactoryGirl.create(:activity_record, user: user)
       get :index
-      expect(assigns(:activity_records)).to eq([record])
-    end
 
-    it "renders the :index view" do
-      get :index
+      expect(assigns(:activity_records)).to eq([record])
       expect(response).to render_template(:index)
     end
   end
 
   describe "GET #show" do
-    it "assigns the request activity to @activity" do
+    it "render the record" do
       activity = ActivityRecord.create
       get :show, id: activity
-      expect(assigns(:activity_record)).to eq(activity)
-    end
 
-    it "renders the :show view" do
-      activity = ActivityRecord.create
-      get :show, id: activity
+      expect(assigns(:activity_record)).to eq(activity)
       expect(response).to render_template(:show)
     end
   end
 
   describe "DELETE #destroy" do
-    before :each do
-      @activity = ActivityRecord.create
-    end
-
     it "deletes the activity record" do
-      expect {
-        delete :destroy, id: @activity
-      }.to change(ActivityRecord, :count).by(-1)
-    end
+      activity = ActivityRecord.create
 
-    it "redirects to activityrecords#index" do
-      delete :destroy, id: @activity
+      expect { delete :destroy, id: activity }.to change(ActivityRecord, :count).by(-1)
       expect(response).to redirect_to admin_activity_records_path
     end
   end
