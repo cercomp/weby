c60497a46685a1cf1df2a1fbb094cbacfafff205
leo-souza <porkinhu@gmail.com>
2018-11-08 15:49:37 -0200

    Add style to news view

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 module SitesHelper
   def render_social_share type
-    html = ''
+    html = '<div class="social-buttons">'
     if current_site.send("#{type}_social_share_networks".to_sym).to_a.include?('twitter')
       html += '<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>'
       html += "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
@@ -21,6 +21,7 @@ module SitesHelper
         <div class="fb-like" data-href="" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 EOF
     end
+    html += '</div>'
     html.html_safe
   end
 end
