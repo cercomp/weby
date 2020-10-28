46ca7e84b74fc92d85e10a3d7dba30026a2addd7
Leonardo R Souza <porkinhu@gmail.com>
2020-10-28 13:49:46 -0300

    fix error on feedback component

--- vendor/engines/feedback/lib/weby/components/feedback/feedback_component.rb
+++ vendor/engines/feedback/lib/weby/components/feedback/feedback_component.rb
@@ -1,9 +1,11 @@
 class FeedbackComponent < Component
-  component_settings :label, :groups_id
+  component_settings :label, :groups_id, :html_class
 
   i18n_settings :label
 
   validate :label_present
+
+  validates :html_class, format: { with: /\A[A-Za-z0-9_\-\s]*\z/ }
 
   alias_method :_groups_id, :groups_id
   def groups_id
