0028ac1ac62140d6ef2ba3b83b0861354a4c2613
Leonardo R Souza <porkinhu@gmail.com>
2020-11-27 11:29:59 -0300

    adds a data migration to add new langs

---
+++ db/data_migrations/add_langs.rb
@@ -0,0 +1,3 @@
+Locale.create!(name: 'zh-CN', flag: 'China.png') unless Locale.exists?(name: 'zh-CN')
+Locale.create!(name: 'it',    flag: 'Italy.png') unless Locale.exists?(name: 'it')
+Locale.create!(name: 'de',    flag: 'Germany.png') unless Locale.exists?(name: 'de')
