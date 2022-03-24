c99b9a502d2903f9393896d5caa7ae4380f2a372
leo-souza <porkinhu@gmail.com>
2022-03-24 16:42:46 -0300

    Adds slug hint

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,3 +2,9 @@ Paperclip.interpolates :site_id do |attachment, _style|
   attachment.instance.site_id
 end
 Paperclip.options[:whiny] = (ENV['PAPERCLIP_WHINY'].to_s == 'true')
+
+Paperclip.options[:content_type_mappings] = {
+  pem: "text/plain",
+  cer: "text/plain",
+  crt: "text/plain"
+}
