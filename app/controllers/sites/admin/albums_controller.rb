798f48231dabf0566892928d88112e46d79e785e
Leonardo R Souza <leonardosouza@ufg.br>
2023-02-03 19:25:38 -0300

    tweaks to photo albums

--- app/controllers/sites/admin/albums_controller.rb
+++ app/controllers/sites/admin/albums_controller.rb
@@ -3,7 +3,7 @@ class Sites::Admin::AlbumsController < ApplicationController
 
   before_action :require_user
   before_action :check_authorization
-  before_action :find_album, only: [:show, :edit, :update, :photos]
+  before_action :find_album, only: [:show, :edit, :update, :destroy, :photos]
 
   helper_method :sort_column
 
@@ -31,28 +31,10 @@ class Sites::Admin::AlbumsController < ApplicationController
     end
   end
 
-  # def get_pages
-  #   case params[:template]
-  #   when 'tiny_mce'
-  #     params[:per_page] = 7
-  #   end
-  #   params[:direction] ||= 'desc'
-  #   # Vai ao banco por linha para recuperar
-  #   # tags e locales
-  #   pages = current_site.pages.includes(:user)
-  #   if params[:template] == 'list_popup'
-  #     pages = pages.published
-  #   end
-  #     pages.with_search(params[:search], 1) # 1 = busca com AND entre termos
-  #     .order(sort_column + ' ' + sort_direction)
-  #     .page(params[:page]).per(params[:per_page])
-  # end
-  # private :get_pages
-
-  # def sort_column
-  #   params[:sort] || 'pages.id'
-  # end
-  # private :sort_column
+  def sort_column
+    params[:sort] || 'pages.id'
+  end
+  private :sort_column
 
   def show
     @album = @album.in(params[:show_locale])
@@ -79,32 +61,32 @@ class Sites::Admin::AlbumsController < ApplicationController
     respond_with(:photos, :site_admin, @album)
   end
 
-  # PUT /pages/1
-  # PUT /pages/1.json
   def update
     @album.update(album_params)
     record_activity('updated_album', @album)
     respond_with(:site_admin, @album)
   end
 
-  # # DELETE /pages/1
-  # # DELETE /pages/1.json
-  # def destroy
-  #   @page = current_site.pages.unscoped.find(params[:id])
-  #   if @page.trash
-  #     if @page.persisted?
-  #       record_activity('moved_page_to_recycle_bin', @page)
-  #       flash[:success] = t('moved_page_to_recycle_bin')
-  #     else
-  #       record_activity('destroyed_page', @page)
-  #       flash[:success] = t('successfully_deleted')
-  #     end
-  #   else
-  #     flash[:error] = @page.errors.full_messages.join(', ')
-  #   end
+  def destroy
+    if @album.destroy
+      record_activity('destroyed_album', @album)
+      flash[:success] = t('successfully_deleted')
+    else
+      flash[:error] = @album.errors.full_messages.join(', ')
+    end
 
-  #   redirect_to @page.persisted? ? site_admin_pages_path : recycle_bin_site_admin_pages_path
-  # end
+    redirect_to site_admin_albums_path
+  end
+
+  def destroy_many
+    albums = current_site.albums.where(id: params[:ids].split(',')).each do |album|
+      if album.destroy
+        record_activity('destroyed_album', album)
+        flash[:success] = t('successfully_deleted')
+      end
+    end
+    redirect_back(fallback_location: site_admin_albums_path)
+  end
 
   # def recover
   #   @page = current_site.pages.trashed.find(params[:id])
@@ -114,17 +96,6 @@ class Sites::Admin::AlbumsController < ApplicationController
   #   record_activity('restored_page', @page)
   #   redirect_back(fallback_location: recycle_bin_site_admin_pages_path)
   # end
-
-  # def destroy_many
-  #   pages = current_site.pages.where(id: params[:ids].split(',')).each do |page|
-  #     if page.trash
-  #       record_activity('moved_page_to_recycle_bin', page)
-  #       flash[:success] = t('moved_page_to_recycle_bin')
-  #     end
-  #   end
-  #   redirect_back(fallback_location: site_admin_pages_path)
-  # end
-
 
   # def empty_bin
   #   if current_site.pages.trashed.destroy_all
