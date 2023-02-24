cd6cdfaa258227afbda0c5eb2ab059b76ea93529
Leonardo R Souza <leonardosouza@ufg.br>
2023-02-24 14:58:52 -0300

    Use a custom name for session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,6 +6,7 @@ check_database = begin
   else
     true
   end
-tld_length = check_database && Setting.table_exists? ? (Weby::Settings::Weby.tld_length.to_i + 1) : 2
+tld_length  = check_database && Setting.table_exists? ? (Weby::Settings::Weby.tld_length.to_i + 1) : 2
+cookie_name = check_database && Setting.table_exists? && Weby::Settings::Weby.domain.present? ? Weby::Settings::Weby.domain.parameterize(separator: '_') : 'weby3'
 
-Rails.application.config.session_store :cookie_store, key: '_weby3_session', domain: :all, tld_length: tld_length
+Rails.application.config.session_store :cookie_store, key: "_#{cookie_name}_sess", domain: :all, tld_length: tld_length
