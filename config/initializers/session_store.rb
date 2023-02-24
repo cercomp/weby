163e830047db2e0af5a82b1d0b9420e90477848a
Leonardo R Souza <leonardosouza@ufg.br>
2023-02-24 11:59:50 -0300

    Fix to catch all errors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 check_database = begin
-  ActiveRecord::Base.connection
-rescue ActiveRecord::NoDatabaseError
-  false
-else
-  true
-end
+    ActiveRecord::Base.connection
+  rescue => err #ActiveRecord::NoDatabaseError, PG::ConnectionBad
+    false
+  else
+    true
+  end
 tld_length = check_database && Setting.table_exists? ? (Weby::Settings::Weby.tld_length.to_i + 1) : 2
 
 Rails.application.config.session_store :cookie_store, key: '_weby3_session', domain: :all, tld_length: tld_length
