6310d55e4df68d7fae57896501ead54061d53a9e
Leonardo R Souza <leonardosouza@ufg.br>
2023-03-13 16:02:11 -0300

    Updates to albu photos

--- app/controllers/sites/album_photos_controller.rb
+++ app/controllers/sites/album_photos_controller.rb
@@ -1,13 +1,19 @@
 class Sites::AlbumPhotosController < ApplicationController
   layout :choose_layout
 
-  before_action :find_album, only: :show
+  before_action :find_album, only: [:show, :download]
 
   respond_to :html, :js, :json
 
   def show
     raise ActiveRecord::RecordNotFound if !@album.publish && @album.user != current_user
     @album_photo = @album.album_photos.find(params[:id])
+  end
+
+  def download
+    raise ActiveRecord::RecordNotFound if !@album.publish && @album.user != current_user
+    @album_photo = @album.album_photos.find(params[:id])
+    redirect_to @album_photo.image.url(:o)
   end
 
 end
