6a3f10670ee6e979238f32dced353db78f96c3d8
Marcello Moura <marcello@ufg.br>
2023-03-25 00:41:50 +0000

    Atualizar app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -128,7 +128,7 @@ class ApplicationController < ActionController::Base
   def set_tld_length
     if current_site && request.domain
       if current_site.domain.present? && request.domain.match(current_site.domain)
-        request.session_options[:tld_length] = current_site.domain.split('.').length + 1
+        request.session_options[:tld_length] = current_site.domain.split('.').length
         request.session_options[:domain] = request.domain
       elsif Weby::Settings::Weby.domain.present? && !(request.domain.match(Weby::Settings::Weby.domain))
         request.session_options[:tld_length] = current_site.domain.split('.').length + 1 if current_site.domain
