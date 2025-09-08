b2dc8f4f0c3eeac610bf9f613392f0501056673e
luis.varella <luis.varella@ufg.br>
2025-09-08 09:54:32 -0300

    Correção: busca de álbum por slug no parâmetro correto e fallback para ambiente local
    - Ajusta método find_album para usar params[:album_id] (conforme rota)
    - Permite funcionamento tanto com subdomínio (produção) quanto sem (localhost)
    - Garante que o botão de download e visualização de fotos funcionem em todos os ambientes

--- app/controllers/sites/album_photos_controller.rb
+++ app/controllers/sites/album_photos_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Sites::AlbumPhotosController < ApplicationController
   layout :choose_layout
 
@@ -13,7 +15,39 @@ class Sites::AlbumPhotosController < ApplicationController
   def download
     raise ActiveRecord::RecordNotFound if !@album.publish && @album.user != current_user
     @album_photo = @album.album_photos.find(params[:id])
-    redirect_to @album_photo.image.url(:o)
+
+    image = @album_photo.image
+    filename = image.original_filename || "foto_#{@album_photo.id}.jpg"
+
+    if image.options[:storage] == :s3
+      # Para S3, lê o arquivo da URL e envia os dados
+      data = URI.open(image.url(:o))
+      send_data data.read, filename: filename, disposition: 'attachment', type: image.content_type
+    else
+      # Para arquivos locais, envia o arquivo diretamente
+      file_path = image.path(:o)
+      if File.exist?(file_path)
+        send_file file_path, filename: filename, disposition: 'attachment', type: image.content_type
+      else
+        # Se o arquivo não for encontrado, retorna um erro 404
+        raise ActiveRecord::RecordNotFound
+      end
+    end
   end
 
+  private
+
+  def find_album
+    site = if request.subdomain.present?
+      Site.find_by(name: request.subdomain)
+    else
+      Site.first
+    end
+    raise ActiveRecord::RecordNotFound unless site
+    @album = site.albums.find_by(slug: params[:album_id])
+  end
+
+  def choose_layout
+    # Lógica para escolher o layout
+  end
 end
