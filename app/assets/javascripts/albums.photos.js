9f34f0da1c74eb91c3587a14dd63e583e6790682
Luis Varella <luis.varella@ufg.br>
2025-11-18 08:22:41 -0300

    feat: Adiciona funcionalidade de marcação para exclusão de fotos e sobreposição visual

--- app/assets/javascripts/albums.photos.js
+++ app/assets/javascripts/albums.photos.js
@@ -3,77 +3,43 @@
 //= require fileupload/jquery.fileupload
 //= require_self
 $(function () {
-  console.log('=== ALBUMS.PHOTOS.JS LOADED ===');
-  console.log('jQuery version:', $.fn.jquery);
-  console.log('FileUpload plugin available:', typeof $.fn.fileupload !== 'undefined');
-  console.log('Template element:', $('.repo-template').length);
-  console.log('Form element:', $('form.new_album_photo').length);
-  console.log('Upload preview container:', $('#upload-preview').length);
-
-  ///Retirar o div invisivel de template para fora da tag form
   $('.repo-template').insertAfter($('form.new_album_photo'));
-  console.log('Template moved outside form');
-
-  // Verificar estado inicial das fotos ao carregar a página
   setTimeout(function() {
     check_uploads();
   }, 100);
-
-  ////Evento do click do botão de excluir o arquivo
   $('#upload-preview').on('click', '.close', function(){
     $(this).parents('.closeable').fadeOut(function(){
       $(this).remove();
       check_uploads();
     })
   });
+  $('#current-photos').on('click', '.close', function(e){
+    e.preventDefault();
+    let $photoElement = $(this).closest('.album-photo');
 
-    $('#current-photos').on('ajax:success', '.close', function(e, data, status, xhr) {
-    console.log('=== PHOTO REMOVED SUCCESSFULLY ===');
-    data = JSON.parse(data);
-    var $photoElement = $(this).closest('.repo-item');
-
-    if ($photoElement.length > 0) {
-      $photoElement.fadeOut(function(){
-        $(this).remove();
-        console.log('Photo element removed, recalculating...');
-        check_uploads();
-      });
+    if ($photoElement.hasClass('marked-for-deletion')) {
+      $photoElement.removeClass('marked-for-deletion');
+      $photoElement.find('.deletion-overlay').remove();
     } else {
-      console.error('Não foi possível encontrar o elemento .repo-item para remover');
-      location.reload();
+      $photoElement.addClass('marked-for-deletion');
+      $photoElement.append('<div class="deletion-overlay">MARCADA PARA EXCLUSÃO</div>');
     }
-
-    if (data.photo_album.is_cover) {
-      $('#album_cover_photo_attributes_id').val(null);
-      $('.cover-preview img').attr('href', '');
-      $('.cover-preview-cont').addClass('hide');
-      $('.cover-preview-cont .file-name').text('');
-    }
-  }).on('ajax:error', '.close', function(e, xhr, status, error) {
-    console.log('Erro ao remover foto:', error);
-    console.log('Status:', xhr.status);
-    console.log('Response:', xhr.responseText);
-    try {
-      var errors = JSON.parse(xhr.responseText).errors;
-      FlashMsg.error(errors, '#tab-photos');
-    } catch(e) {
-      FlashMsg.error(['Erro ao remover a foto. Tente novamente.'], '#tab-photos');
-    }
-  }).on('ajax:success', '.edit_album_photo', function(e, data, status, xhr) {
+  });
+  $('#current-photos').on('ajax:success', '.edit_album_photo', function(e, data, status, xhr) {
     $(this).find('.save-btn').addClass('hide');
   }).on('ajax:error', '.edit_album_photo', function(e, xhr, status, error) {
     console.log(error);
   });
 
   $('#current-photos').on('keyup', '[name="album_photo[description]"]', function(e){
-    var form = $(this).closest('form')
+    let form = $(this).closest('form')
     form.find('.save-btn').removeClass('hide');
     form.find('.status').text(null)
   });
 
   $('#current-photos').on('click', '.make-cover', function(e){
-    $item = $(this).closest('.album-photo');
-    $form = $item.find('form');
+    let $item = $(this).closest('.album-photo');
+    let $form = $item.find('form');
     $.post($form.attr('action'), {make_cover: true, _method: 'patch'}, function(data){
       $('.is-cover').remove()
       $item.replaceWith(data.html);
@@ -91,32 +57,21 @@ $(function () {
     const currentPhotosCount = $('#current-photos .repo-item:visible').length;
     const maxPhotosPerUpload = 100;
 
-    console.log('=== CHECK_UPLOADS ===');
-    console.log('Preview count (new photos):', previewCount);
-    console.log('Current photos count:', currentPhotosCount);
-    console.log('Max per upload:', maxPhotosPerUpload);
-
-    // Limpar mensagens de erro antigas
     FlashMsg.clear();
 
-    // Atualizar contador visual
     updatePhotosCounter(previewCount, currentPhotosCount, maxPhotosPerUpload);
 
     if (previewCount > 0) {
       console.log('Has items in preview');
-      // Verificar se excede o limite de 100 fotos por upload
       if (previewCount > maxPhotosPerUpload) {
-        console.log('EXCEEDED LIMIT!');
         FlashMsg.error([`É possível fazer o upload de até ${maxPhotosPerUpload} fotos por vez. Você selecionou ${previewCount} foto(s). Por favor, remova ${previewCount - maxPhotosPerUpload} foto(s).`], '#tab-photos');
         $('form.new_album_photo .form-actions').addClass('hide');
       } else {
         console.log('Within limit, showing submit button');
-        // Dentro do limite, mostrar botão de envio
         $('form.new_album_photo .form-actions').removeClass('hide');
       }
     } else {
       console.log('No items in preview, hiding submit button');
-      // Sem fotos no preview, esconder botão
       $('form.new_album_photo .form-actions').addClass('hide');
     }
 
@@ -124,7 +79,6 @@ $(function () {
   }
 
   function updatePhotosCounter(previewCount, currentPhotosCount, maxPhotosPerUpload) {
-    // Criar ou atualizar o contador visual
     let $counter = $('#photos-counter');
     if ($counter.length === 0) {
       $counter = $('<div id="photos-counter" style="margin: 10px 0; padding: 8px; background: #f5f5f5; border-radius: 4px; font-size: 14px;"></div>');
@@ -148,29 +102,26 @@ $(function () {
   }
 
   function switch_disable_text(disable){
-    var $submit = $('.send-files');
-    var $dis_txt = $submit.val();
+    let $submit = $('.send-files');
+    let $dis_txt = $submit.val();
     $submit.val($submit.data('disable-with')).data('disable-with', $dis_txt).prop('disabled', disable);
   }
 
   function handleFail(context, errors){
-    var $msg = context.find('.status');
+    let $msg = context.find('.status');
     $msg.html(null);
-    for(var idx in errors){
+    for(let idx in errors){
        $msg.append('<span class="label label-important">'+errors[idx]+'</span>&nbsp;');
     }
   }
-
-  ////Não envia o submit do form principal, e chama o data.submit de cada arquivo incluido
   $('form.new_album_photo').submit(function(){
-    $this = $(this);
+    let $this = $(this);
     if($this.find('.repo-item').length == 0){
       return false;
     }
     $this.find('.repo-item').each(function(){
       $(this).find('.status').html('<img src="'+assetPath('loading-bar.gif')+'"/>').addClass('loading');
-      var $data = $(this).data('dataobj');
-      //console.log($data);
+      let $data = $(this).data('dataobj');
       $data.formData = {"album_photo[description]" : $(this).find('#album_photo_description').val()};
       $data.submit();
     });
@@ -183,37 +134,17 @@ $(function () {
     paramName: 'album_photo[image]',
     dataType: 'json',
     url: $('form.new_album_photo').prop('action') + '.json',
-    ////Evento de inclusão de arquivo, chamado para cada arquivo selecionado
     add: function (e, data) {
-      console.log('=== ADD EVENT TRIGGERED ===');
-      console.log('File name:', data.files[0].name);
-      console.log('File type:', data.files[0].type);
-      console.log('File size:', data.files[0].size);
-
-      // Validação de arquivos duplicados removida do preview
-      // A validação agora acontece no backend durante o upload
-      // conforme solicitado pelo analista
-
-      console.log('Cloning template...');
-      var $repoItem = $('.repo-template').clone(true);
-      console.log('Template cloned:', $repoItem.length);
-
+      let $repoItem = $('.repo-template').clone(true);
       $repoItem.removeClass('repo-template').addClass('repo-item').show();
       $repoItem.find('#album_photo_image').val(data.files[0].name);
       $repoItem.find('.file-name').text(data.files[0].name);
 
-      /////Geração do thumbnail de preview (Se o browser tiver o FileReader)
-      console.log('Checking FileReader support...');
-      console.log('FileReader exists:', typeof FileReader !== "undefined");
-      console.log('Is image:', (/image/i).test(data.files[0].type));
-
       if (typeof FileReader !== "undefined" && (/image/i).test(data.files[0].type)) {
-        console.log('Creating image preview...');
-        var img = document.createElement('img');
-        var reader = new FileReader();
+        let img = document.createElement('img');
+        let reader = new FileReader();
 
         reader.onload = function(evt){
-          console.log('Image loaded successfully');
           img.src = evt.target.result;
         };
 
@@ -227,24 +158,17 @@ $(function () {
       } else {
         console.warn('FileReader not supported or file is not an image');
       }
-
-      console.log('Appending to #upload-preview...');
       $repoItem.appendTo($('#upload-preview'));
       $repoItem.data('dataobj', data);
 
       data.context = $repoItem;
-
-      console.log('Items in preview:', $('#upload-preview .repo-item:not(.repo-template)').length);
       check_uploads();
     },
-    /////Evento de retorno do processamento, executado para cada envio, executando tanto success ou failure
     always: function (e, data) {
-      console.log('=== UPLOAD ALWAYS (after success/fail) ===');
-      var $msg = data.context.find('.status');
+      let $msg = data.context.find('.status');
       $msg.removeClass('loading');
       if($('.status.loading').length == 0){
         switch_disable_text(false);
-        console.log('All uploads finished, recalculating...');
       }
       check_uploads();
     },
@@ -253,31 +177,25 @@ $(function () {
         if(data.result.errors){
            handleFail(data.context, data.result.errors);
         }else{
-          console.log('=== UPLOAD DONE SUCCESSFULLY ===');
-          var $repoItem = data.context;
+          let $repoItem = data.context;
           $repoItem.remove();
-          var html = $(data.result.html)
+          let html = $(data.result.html)
           html.find('.status').html('<span class="label label-success">'+data.result.message+'</span>');
-          container = $('<div class="'+ $('.repo-template')[0].className +'"></div>')
-          container.removeClass('repo-template').addClass('repo-item')
+          let container = $('<div class="'+ $('.repo-template')[0].className +'"></div>')
+          container.removeClass('repo-template').addClass('repo-item uploaded-in-session')
+          container.attr('data-upload-time', Date.now());
           $('#current-photos').append(container.html(html));
-          console.log('Photo added to current-photos, recalculating...');
           check_uploads();
         }
      },
 
      fail: function(e, data) {
-         console.log('Upload failed:', e);
-         console.log('Data:', data);
-         console.log('Response status:', data.jqXHR.status);
-         console.log('Response text:', data.jqXHR.responseText);
-
         try{
-          var response = JSON.parse(data.jqXHR.responseText);
-          var errors = response.errors || ['Erro ao enviar a foto. Tente novamente.'];
+          let response = JSON.parse(data.jqXHR.responseText);
+          let errors = response.errors || ['Erro ao enviar a foto. Tente novamente.'];
         }catch(e){
           console.log('Error parsing response:', e);
-          var errors = [data.jqXHR.responseText.split(/\r?\n/)[0] || 'Erro desconhecido ao enviar a foto.']
+          let errors = [data.jqXHR.responseText.split(/\r?\n/)[0] || 'Erro desconhecido ao enviar a foto.']
         }
         handleFail(data.context, errors)
      }
@@ -287,13 +205,126 @@ $(function () {
     $('#current-photos .save-btn:not(.hide)').each(function(){
       $(this).find('[type=submit]').click();
     });
-    $('form.edit_album').submit();
+    const markedForDeletion = $('#current-photos .album-photo.marked-for-deletion');
+    if (markedForDeletion.length > 0) {
+      let processedCount = 0;
+      const totalCount = markedForDeletion.length;
+
+      markedForDeletion.each(function() {
+        const $photo = $(this);
+        const $deleteBtn = $photo.find('.close');
+
+        if ($deleteBtn.length > 0) {
+          $deleteBtn.attr('data-remote', 'true');
+          $deleteBtn.attr('data-method', 'delete');
+          const photoId = $photo.find('form').attr('id').replace('edit_album_photo_', '');
+          $deleteBtn.attr('href', `/admin/albums/${window.location.pathname.split('/')[3]}/album_photos/${photoId}`);
+          $photo.one('ajax:success', function(e, data, status, xhr) {
+            processedCount++;
+            $photo.fadeOut(function() {
+              $(this).remove();
+            });
+
+            if (processedCount >= totalCount) {
+              $('form.edit_album').submit();
+            }
+          });
+          $photo.one('ajax:error', function() {
+            processedCount++;
+
+            if (processedCount >= totalCount) {
+              $('form.edit_album').submit();
+            }
+          });
+          $deleteBtn.trigger('click');
+        } else {
+          processedCount++;
+          if (processedCount >= totalCount) {
+            $('form.edit_album').submit();
+          }
+        }
+      });
+    } else {
+      $('form.edit_album').submit();
+    }
+
     return false;
   });
 
-  const input = document.querySelector('#album_photo_image');
+  $('.form-actions .btn-default').click(function(e) {
+    const recentUploads = $('.uploaded-in-session');
+    const markedForDeletion = $('#current-photos .album-photo.marked-for-deletion');
 
-  // Listen for files selection
+    if (recentUploads.length > 0 || markedForDeletion.length > 0) {
+      e.preventDefault();
+
+      let message = '';
+      if (recentUploads.length > 0 && markedForDeletion.length > 0) {
+        message = `Você tem ${recentUploads.length} foto(s) enviada(s) nesta sessão e ${markedForDeletion.length} foto(s) marcada(s) para exclusão. Deseja cancelar e remover essas mudanças?`;
+      } else if (recentUploads.length > 0) {
+        message = `Você tem ${recentUploads.length} foto(s) enviada(s) nesta sessão. Deseja cancelar e remover essas fotos?`;
+      } else {
+        message = `Você tem ${markedForDeletion.length} foto(s) marcada(s) para exclusão. Deseja cancelar e manter essas fotos?`;
+      }
+
+      if (confirm(message)) {
+        markedForDeletion.each(function() {
+          $(this).removeClass('marked-for-deletion');
+          $(this).find('.deletion-overlay').remove();
+        });
+        let processedCount = 0;
+        const totalCount = recentUploads.length;
+        const redirectUrl = $(this).attr('href');
+
+        if (totalCount === 0) {
+          globalThis.location.href = redirectUrl;
+          return;
+        }
+
+        recentUploads.each(function() {
+          const $photo = $(this);
+          const $deleteBtn = $photo.find('.close');
+
+          if ($deleteBtn.length > 0) {
+            $deleteBtn.attr('data-remote', 'true');
+            $deleteBtn.attr('data-method', 'delete');
+            const photoId = $photo.find('form').attr('id').replace('edit_album_photo_', '');
+            $deleteBtn.attr('href', `/admin/albums/${window.location.pathname.split('/')[3]}/album_photos/${photoId}`);
+            $photo.one('ajax:success', function() {
+              processedCount++;
+              console.log(`Recent upload removed: ${processedCount}/${totalCount}`);
+
+              if (processedCount >= totalCount) {
+                console.log('All changes cancelled, redirecting...');
+                setTimeout(function() {
+                  globalThis.location.href = redirectUrl;
+                }, 500);
+              }
+            });
+            $photo.one('ajax:error', function() {
+              processedCount++;
+              console.log(`Recent upload removal failed: ${processedCount}/${totalCount}`);
+
+              if (processedCount >= totalCount) {
+                console.log('Processing finished (with errors), redirecting...');
+                setTimeout(function() {
+                  globalThis.location.href = redirectUrl;
+                }, 500);
+              }
+            });
+            $deleteBtn.trigger('click');
+          } else {
+            processedCount++;
+            if (processedCount >= totalCount) {
+              globalThis.location.href = redirectUrl;
+            }
+          }
+        });
+      }
+    }
+  });
+
+  const input = document.querySelector('#album_photo_image');
   if (input) {
     console.log('File input found, attaching listener');
     input.addEventListener('change', (e) => {
@@ -302,14 +333,12 @@ $(function () {
         for(let i = 0; i < e.target.files.length; i++) {
           console.log(`File ${i+1}:`, e.target.files[i].name, e.target.files[i].type);
         }
-        // A validação será feita pela função check_uploads() após a adição dos arquivos
     });
   } else {
     console.error('File input #album_photo_image not found!');
   }
 
   $("input[type='submit']").click(function(){
-    // A validação já é feita pela função check_uploads() que controla a visibilidade do botão
     return check_uploads_and_submit();
   });
 
